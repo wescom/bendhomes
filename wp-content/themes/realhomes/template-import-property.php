96e3a4455fa201db3199f8218cc8dd01f68dc212
Justin Grady <justingrady@gmail.com>
2016-03-23 16:00:06 -0700

    template import test

--- wp-content/themes/realhomes/template-import-property.php
+++ wp-content/themes/realhomes/template-import-property.php
@@ -205,7 +205,7 @@ function dbresult() {
   $sqlquery = "SELECT * FROM ".$rc." WHERE
               PublishToInternet = 1
               AND lastPullTime >= '".$querydate."'
-              LIMIT 5
+              AND Status = 'Active'
               ;";
 
   echo '<pre>';
