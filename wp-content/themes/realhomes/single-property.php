5819c06227e1c9f89a10473874804bbc4638b87d
Justin Grady <justingrady@gmail.com>
2016-04-09 14:38:32 -0700

    updates - ad pos

--- wp-content/themes/realhomes/single-property.php
+++ wp-content/themes/realhomes/single-property.php
@@ -12,7 +12,8 @@ if( $banner_image_id ){
     $banner_image_path = get_default_banner();
 }
 ?>
-
+    <?php // 1777 -- removal of graphic head area on property detail view
+    /*
     <div class="page-head" style="background-repeat: no-repeat;background-position: center top;background-image: url('<?php echo $banner_image_path; ?>'); background-size: cover;">
         <?php if(!('true' == get_option('theme_banner_titles'))): ?>
             <div class="container">
@@ -28,17 +29,32 @@ if( $banner_image_id ){
             </div>
         <?php endif; ?>
     </div><!-- End Page Head -->
+    */
+    ?>
+
+    <div class="page-spacer"></div>
 
     <!-- Content -->
     <div class="container contents detail">
         <div class="row">
             <div class="span9 main-wrap">
-
+              <?php
+              // 1777
+              $display_property_breadcrumbs = get_option( 'theme_display_property_breadcrumbs' );
+              if ( $display_property_breadcrumbs == 'true' ) {
+                  get_template_part( 'property-details/property-breadcrumbs' );
+              }
+              ?>
                 <!-- Main Content -->
                 <div class="main">
 
                     <div id="overview">
                         <?php
+                        // 1777
+                        echo '<h1 class="property-title">';
+                        the_title();
+                        echo '</h1>';
+
                         if ( have_posts() ) :
                             while ( have_posts() ) :
                                 the_post();
@@ -64,7 +80,7 @@ if( $banner_image_id ){
                                     * 2. Property Information Bar, Icons Bar, Text Contents and Features
                                     */
                                     get_template_part('property-details/property-contents');
-									
+
                                     /*
                                     * 3. Property Floor Plans
                                     */
