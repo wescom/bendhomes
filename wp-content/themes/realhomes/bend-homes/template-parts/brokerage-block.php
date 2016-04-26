4e696ccc429981173332710066ea3deb2d6451ea
Justin Grady <justingrady@gmail.com>
2016-04-26 12:44:41 -0700

    spin off property agent for sidebar file for bend-homes use

--- wp-content/themes/realhomes/bend-homes/template-parts/brokerage-block.php
+++ wp-content/themes/realhomes/bend-homes/template-parts/brokerage-block.php
@@ -8,8 +8,6 @@ $brokerage = array(
 );
 */
 
-echo 'test3333';
-
 $brokerage = array(
   'name' => get_post_meta($my_id, 'brk_office_name',true),
   'address' => get_post_meta($my_id, 'brk_office_address',true),
