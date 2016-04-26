159bc5442761ca8cb0a4e5cb0adc7ae8e6c5d8cb
Justin Grady <justingrady@gmail.com>
2016-04-26 13:43:45 -0700

    spin off property agent for sidebar file for bend-homes use

--- wp-content/themes/realhomes/bend-homes/property-details/property-agent-for-sidebar.php
+++ wp-content/themes/realhomes/bend-homes/property-details/property-agent-for-sidebar.php
@@ -76,7 +76,7 @@ function display_sidebar_agent_box( $args ) {
 			</ul>
       <?php brokerageBlock($args[ 'agent_id' ]); ?>
 			<p><?php
-				echo $args[ 'agent_description' ]; ?><br/><?php
+				echo $args[ 'agent_description' ];
 				if ( isset( $args[ 'display_author' ] ) && ( $args[ 'display_author' ] ) ) {
 					?><a class="real-btn" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php _e( 'Know More', 'framework' ); ?></a><?php
 				} else {
