10864c52d57605907932e8e534b440891e4fde80
Justin Grady <justingrady@gmail.com>
2016-04-28 13:32:10 -0700

    css tweaks

--- wp-content/themes/realhomes/framework/functions/basic.php
+++ wp-content/themes/realhomes/framework/functions/basic.php
@@ -417,7 +417,7 @@ if ( ! function_exists( 'get_inspiry_image_placeholder' ) ) {
 		$holder_width = 0;
 		$holder_height = 0;
 		// $holder_text = get_bloginfo( 'name' );
-		$holder_text = 'image not available';
+		$holder_text = 'image not available dude';
 		$holder_text_size = 30;
 
 		if ( in_array( $image_size, array( 'thumbnail', 'medium', 'large' ) ) ) {
