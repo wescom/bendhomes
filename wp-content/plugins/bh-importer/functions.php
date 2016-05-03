accfd871ff2cfa66590d1e7fb05e267cb58424bf
Justin Grady <justingrady@gmail.com>
2016-05-03 00:22:59 -0700

    update function for prop date

--- wp-content/plugins/bh-importer/functions.php
+++ wp-content/plugins/bh-importer/functions.php
@@ -488,18 +488,18 @@ function dbresult($sset) {
   */
 
   // daily cron query
-  /*
+
   $sqlquery = "SELECT * FROM ".$rc." WHERE
               PublishToInternet = 1
               AND lastPullTime >= '".$querydate."'
               ;";
-*/
   // used for initial pull, nuclear option to get all data
-  
+  /*
   $sqlquery = "SELECT * FROM ".$rc." WHERE
               PublishToInternet = 1
               AND Status = 'Active'
               ;";
+              */
 
 
   echo '<pre>';
