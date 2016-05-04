b5bf86a454d49ab3df45f380867a1578af67246b
Justin Grady <justingrady@gmail.com>
2016-05-04 11:37:32 -0700

    gfrom template part add

---
+++ wp-content/themes/realhomes/bend-homes/template-parts/gform-signup.php
@@ -0,0 +1,53 @@
+<p>Sign up today to receive special alerts, listings, and knowledge articles about real estate in Central Oregon</p>
+
+<div class="fusion-two-fifth fusion-layout-column fusion-column-last fusion-spacing-yes" style="margin-top:0px;margin-bottom:20px;">
+  <div class="fusion-column-wrapper fusion-animated" style="padding: 50px 30px; visibility: visible; animation-duration: 0.5s; background-color: rgba(58, 170, 203, 0.74902);" data-animationtype="fadeInRight" data-animationduration="0.5" data-animationoffset="100%">
+    <h2 style="text-align: center; font-size: 46px; font-weight: bold; margin: 0 0 15px; line-height: 46px;"><span style="color: #ffffff;">SIGN UP TODAY</span></h2>
+    <p style="text-align: center;">
+    </p>
+    <div class="gf_browser_chrome gform_wrapper no-labels top-form gform_minify_errors_wrapper" id="gform_wrapper_7">
+      <form method="post" enctype="multipart/form-data" target="gform_ajax_frame_7" id="gform_7" class="no-labels top-form gform_minify_errors" action="/bend-homes/">
+        <div class="gform_body">
+          <ul id="gform_fields_7" class="gform_fields top_label form_sublabel_below description_below">
+            <li id="field_7_1" class="gfield gfield_contains_required field_sublabel_below field_description_below">
+              <label class="gfield_label" for="input_7_1">Name<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_text"><input name="input_1" id="input_7_1" type="text" value="" class="large" tabindex="1" placeholder="Name"></div>
+            </li>
+            <li id="field_7_3" class="gfield gfield_contains_required field_sublabel_below field_description_below">
+              <label class="gfield_label" for="input_7_3">Email<span class="gfield_required">*</span></label>
+              <div class="ginput_container ginput_container_email">
+                <input name="input_3" id="input_7_3" type="email" value="" class="large" tabindex="2" placeholder="Email">
+              </div>
+            </li>
+            <li id="field_7_4" class="gfield gform_hidden field_sublabel_below field_description_below"><input name="input_4" id="input_7_4" type="hidden" class="gform_hidden" value="05/04/2016"></li>
+            <li id="field_7_5" class="gfield gform_hidden field_sublabel_below field_description_below"><input name="input_5" id="input_7_5" type="hidden" class="gform_hidden" value="73.96.60.162"></li>
+            <li id="field_7_6" class="gfield gform_hidden field_sublabel_below field_description_below"><input name="input_6" id="input_7_6" type="hidden" class="gform_hidden" value=""></li>
+            <li id="field_7_7" class="gfield gform_hidden field_sublabel_below field_description_below"><input name="input_7" id="input_7_7" type="hidden" class="gform_hidden" value="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36"></li>
+            <li id="field_7_8" class="gfield gform_validation_container field_sublabel_below field_description_below">
+              <label class="gfield_label" for="input_7_8">Comments</label>
+              <div class="ginput_container"><input name="input_8" id="input_7_8" type="text" value="" autocomplete="off"></div>
+              <div class="gfield_description">This field is for validation purposes and should be left unchanged.</div>
+            </li>
+          </ul>
+        </div>
+        <div class="gform_footer top_label"> <input type="submit" id="gform_submit_button_7" class="gform_button button" value="Sign Me Up" tabindex="3" onclick="if(window[&quot;gf_submitting_7&quot;]){return false;}  if( !jQuery(&quot;#gform_7&quot;)[0].checkValidity || jQuery(&quot;#gform_7&quot;)[0].checkValidity()){window[&quot;gf_submitting_7&quot;]=true;}  "> <input type="hidden" name="gform_ajax" value="form_id=7&amp;title=&amp;description=&amp;tabindex=1">
+          <input type="hidden" class="gform_hidden" name="is_submit_7" value="1">
+          <input type="hidden" class="gform_hidden" name="gform_submit" value="7">
+          <input type="hidden" class="gform_hidden" name="gform_unique_id" value="">
+          <input type="hidden" class="gform_hidden" name="state_7" value="WyJbXSIsImIwNTMwYjg0ZjA1Zjc2OGM4ZmRiNDM0NzlmNTE0MTNjIl0=">
+          <input type="hidden" class="gform_hidden" name="gform_target_page_number_7" id="gform_target_page_number_7" value="0">
+          <input type="hidden" class="gform_hidden" name="gform_source_page_number_7" id="gform_source_page_number_7" value="1">
+          <input type="hidden" name="gform_field_values" value="">
+        </div>
+      </form>
+    </div>
+    <iframe style="display:none;width:0px;height:0px;" src="about:blank" name="gform_ajax_frame_7" id="gform_ajax_frame_7" title="Ajax Frame"></iframe>
+    <script type="text/javascript">jQuery(document).ready(function($){gformInitSpinner( 7, 'http://adhosting.wescompapers.com/wp-content/plugins/gravityforms/images/spinner.gif' );jQuery('#gform_ajax_frame_7').load( function(){var contents = jQuery(this).contents().find('*').html();var is_postback = contents.indexOf('GF_AJAX_POSTBACK') >= 0;if(!is_postback){return;}var form_content = jQuery(this).contents().find('#gform_wrapper_7');var is_confirmation = jQuery(this).contents().find('#gform_confirmation_wrapper_7').length > 0;var is_redirect = contents.indexOf('gformRedirect(){') >= 0;var is_form = form_content.length > 0 && ! is_redirect && ! is_confirmation;if(is_form){jQuery('#gform_wrapper_7').html(form_content.html());setTimeout( function() { /* delay the scroll by 50 milliseconds to fix a bug in chrome */  }, 50 );if(window['gformInitDatepicker']) {gformInitDatepicker();}if(window['gformInitPriceFields']) {gformInitPriceFields();}var current_page = jQuery('#gform_source_page_number_7').val();gformInitSpinner( 7, 'http://adhosting.wescompapers.com/wp-content/plugins/gravityforms/images/spinner.gif' );jQuery(document).trigger('gform_page_loaded', [7, current_page]);window['gf_submitting_7'] = false;}else if(!is_redirect){var confirmation_content = jQuery(this).contents().find('#gforms_confirmation_message_7').html();if(!confirmation_content){confirmation_content = contents;}setTimeout(function(){jQuery('#gform_wrapper_7').replaceWith('<' + 'div id=\'gforms_confirmation_message_7\' class=\'gform_confirmation_message_7 gforms_confirmation_message\'' + '>' + confirmation_content + '<' + '/div' + '>');jQuery(document).trigger('gform_confirmation_loaded', [7]);window['gf_submitting_7'] = false;}, 50);}else{jQuery('#gform_7').append(contents);if(window['gformRedirect']) {gformRedirect();}}jQuery(document).trigger('gform_post_render', [7, current_page]);} );} );</script><script type="text/javascript"> if(typeof gf_global == 'undefined') var gf_global = {"gf_currency_config":{"name":"U.S. Dollar","symbol_left":"$","symbol_right":"","symbol_padding":"","thousand_separator":",","decimal_separator":".","decimals":2},"base_url":"http:\/\/adhosting.wescompapers.com\/wp-content\/plugins\/gravityforms","number_formats":[],"spinnerUrl":"http:\/\/adhosting.wescompapers.com\/wp-content\/plugins\/gravityforms\/images\/spinner.gif"};jQuery(document).bind('gform_post_render', function(event, formId, currentPage){if(formId == 7) {if(typeof Placeholders != 'undefined'){
+      Placeholders.enable();
+      }} } );jQuery(document).bind('gform_post_conditional_logic', function(event, formId, fields, isInit){} );
+    </script><script type="text/javascript"> jQuery(document).ready(function(){jQuery(document).trigger('gform_post_render', [7, 1]) } ); </script>
+    <p></p>
+    <div style="color: #ccc; font-size: 13px; line-height: 150%;"><i class="fa fa-lock"></i> We will never share or sell your information</div>
+    <div class="fusion-clearfix"></div>
+  </div>
+</div>
