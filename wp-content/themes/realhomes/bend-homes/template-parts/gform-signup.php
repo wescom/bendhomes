1957daa2f2b6f845803bf6cb4a16f938ee1b76c2
Justin Grady <justingrady@gmail.com>
2016-05-04 13:14:46 -0700

    mail form

--- wp-content/themes/realhomes/bend-homes/template-parts/gform-signup.php
+++ wp-content/themes/realhomes/bend-homes/template-parts/gform-signup.php
@@ -1,6 +1,7 @@
 
 <?php
 $scripturl = get_template_directory() . '/bend-homes/js/mcproc.js';
+$posturl = "//bendbulletin.us1.list-manage.com/subscribe/post-json?u=a5d36976165603b3ce7485798&amp;id=5e8299d4c6&amp;c=?";
 print_r($scripturl);
 ?>
 <script type='text/javascript' src="<?php echo $scripturl; ?>"></script>
@@ -10,25 +11,17 @@ print_r($scripturl);
       <p>Sign up today to receive special alerts, listings, and knowledge articles about real estate in Central Oregon</p>
     </div>
     <div id="mc_embed_signup" class="clearfix bv-form" novalidate="novalidate">
-        <form action="//burmasphere.us12.list-manage.com/subscribe/post-json?u=42c03db0ed60b21c7c76ebe02&amp;id=dfe8c70d46&amp;c=?" method="get" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate="">
+        <form action="<?php echo $posturl; ?>" method="get" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate="">
 
-            <input type="hidden" name="u" value="42c03db0ed60b21c7c76ebe02">
-            <input type="hidden" name="amp;id" value="dfe8c70d46">
+            <input type="hidden" name="u" value="a5d36976165603b3ce7485798">
+            <input type="hidden" name="amp;id" value="5e8299d4c6">
             <input type="hidden" name="amp;c" value="?">
 
             <div class="form-group has-feedback">
                 <div class="right-inner-addon">
                     <!-- <i class="form-control-feedback" data-bv-icon-for="EMAIL" style="visibility: hidden;"></i> -->
-                    <input type="email" value="" name="EMAIL" class="bm-form-control" id="mce-EMAIL" placeholder="Enter your email address" data-bv-field="EMAIL">
+                    <input type="email" value="" name="EMAIL" class="bh-form-control" id="mce-EMAIL" placeholder="Enter your email address" data-bv-field="EMAIL">
                 </div>
-            </div>
-            <div class="mc-field-group input-group hide"> <strong>Newsletter type: </strong>
-                <ul>
-                    <li>
-                        <input type="checkbox" value="1" checked="checked" name="group[13][1]" id="mce-group[13]-13-0">
-                        <label for="mce-group[13]-13-0">Top Headlines</label>
-                    </li>
-                </ul>
             </div>
             <div class="mc-field-group input-group hide"> <strong>Email Format </strong>
                 <ul>
@@ -44,11 +37,11 @@ print_r($scripturl);
             </div>
             <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
             <div style="position: absolute; left: -5000px;">
-                <input type="text" name="b_42c03db0ed60b21c7c76ebe02_dfe8c70d46" tabindex="-1" value="">
-                <input type="hidden" value="benotifiedform" name="SOURCE" class="" id="mce-SOURCE">
+                <input type="text" name="b_a5d36976165603b3ce7485798_5e8299d4c6" tabindex="-1" value="">
+                <input type="hidden" value="bendhomesfooter" name="SOURCE" class="" id="mce-SOURCE">
             </div>
 
-            <button type="submit" class="btn btn-success bm-form-button" id="mc_sub_button" name="sub_button">Sign Up</button>
+            <button type="submit" class="btn btn-success bh-form-button" id="mc_sub_button" name="sub_button">Sign Up</button>
 
         </form>
         <input type="hidden" name="sub_button" value="">
