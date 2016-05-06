6daae0ffedf85dcd9c3ed16868d2e132940fee96
Justin Grady <justingrady@gmail.com>
2016-05-05 18:22:37 -0700

    orphan image del

---
+++ wp-content/themes/realhomes/template-delete-orphan-images.php
@@ -0,0 +1,101 @@
+<?php
+/*
+*  Template Name: Delete orphan property images
+*/
+/*
+*  Author: Justin Grady
+*/
+
+ini_set('max_execution_time', 0);
+date_default_timezone_set('America/Los_Angeles');
+
+/* #### INCLUDES ##### */
+include_once ABSPATH . 'wp-admin/includes/media.php';
+include_once ABSPATH . 'wp-admin/includes/file.php';
+include_once ABSPATH . 'wp-admin/includes/image.php';
+include_once WP_PLUGIN_DIR . '/'.'bh-importer/functions.php';
+
+
+if ( ! function_exists( 'delete_orphan_images' ) ) {
+  function delete_orphan_images($post_id) {
+    // presets
+    global $wpdb;
+    $imgdir = ABSPATH.'wp-content/uploads/';
+
+    // orphaned images query
+    $sqlquery = "SELECT pm.meta_value
+      FROM $wpdb->postmeta pm
+      LEFT JOIN wp_posts wp ON wp.ID = pm.post_id
+      WHERE wp.ID IS NULL
+      AND pm.meta_key = 'REAL_HOMES_property_images'
+      LIMIT 200
+    ";
+
+    echo '<pre style="background-color: yellow;">';
+    print_r($sqlquery);
+    echo '</pre>';
+
+    // put results into an array
+    $results = $wpdb->get_results( $sqlquery, ARRAY_A );
+    unset($sqlquery);
+
+    foreach($results as $result) {
+      if(strpos($result['meta_value'],'WP_Error')) {
+        // if wp_error, throw it out, continue onto next in loop, no actions
+        continue;
+      } else {
+        $imgid = $result['meta_value'];
+      }
+      // $imgid = $result['meta_value'];
+      // print_r($imgid);
+      // echo "\n";
+
+      if($imgid != NULL) {
+        $sqlquery = "SELECT meta_key, meta_value FROM $wpdb->postmeta WHERE post_id = ".$imgid;
+        echo $sqlquery;
+        $imgpostmetas = $wpdb->get_results( $sqlquery, ARRAY_A );
+      } else {
+        $imgpostmetas = NULL;
+      }
+      unset($sqlquery);
+      // print_r($imgpostmetas);
+
+      echo '<pre style="background-color: #ececec; margin: 10px; borderL 1px solid #cc0000; padding: 10px;">';
+      echo "\n".'<strong style="color: #cc0000">imgage post id: '.$imgid.'</strong>'."\n";
+      foreach($imgpostmetas as $imgpostmeta) {
+        if($imgpostmeta['meta_key'] == '_wp_attached_file' ) {
+          $deletefile = $imgdir.$imgpostmeta['meta_value'];
+          $froot = explode('.',$deletefile);
+          $froot = $froot[0]; // we want of root of the filename with no extension
+          foreach( glob($froot.'*') as $file )
+          {
+              // this deletes all files with the orignal images name pattern, deletes WP versions
+              echo $file;
+              echo "\n";
+              unlink($file);
+          }
+          delete_post_meta($imgid, $imgpostmeta['meta_key']);
+        }
+      }
+      $wpdb->delete( 'wp_postmeta', array(
+        'meta_key' => 'REAL_HOMES_property_images',
+        'meta_value' => $imgid
+        )
+      );
+      // delete_post_meta($imgid, '_wp_attached_file');
+      // delete_post_meta($imgid, '_wp_attachment_metadata');
+      $delpost = wp_delete_post( $imgid );
+      echo '<p style="color: blue;">delete status: ';
+      print_r($delpost);
+      echo '</p>';
+      // print_r($delpost);
+      echo '</pre>';
+    }
+
+  }
+  add_action('delete_orphans', 'delete_orphan_images', 10, 1);
+}
+
+do_action('delete_orphans');
+
+?>
