6da13f2567709dd3e715a84e978a88c98b071bfc
Justin Grady <justingrady@gmail.com>
2016-05-09 12:34:34 -0700

    install google analyitcs

--- wp-content/themes/realhomes/bend-homes/bh-functions.php
+++ wp-content/themes/realhomes/bend-homes/bh-functions.php
@@ -4,4 +4,10 @@ function brokerageBlock($my_id) {
   include( 'template-parts/brokerage-block.php' );
 }
 
+if ( ! function_exists( 'inspiry_theme_setup' ) ) {
+  function add_googleanalytics() {
+    include( 'template-parts/googleanalytics.php' );
+  }
+  add_action('wp_footer', 'add_googleanalytics');
+}
 ?>
