321f698634135a9ef6a1b4fe41600e271841780a
jarelsc <jculley@wescompapers.com>
2016-05-12 14:09:09 -0700

    remove analytics code into plugin

--- wp-content/themes/realhomes/bend-homes/bh-functions.php
+++ wp-content/themes/realhomes/bend-homes/bh-functions.php
@@ -3,13 +3,4 @@
 function brokerageBlock($my_id) {
   include( 'template-parts/brokerage-block.php' );
 }
-
-if ( ! function_exists( 'add_googleanalytics' ) ) {
-  function add_googleanalytics() {
-    echo "\n";
-    include( 'template-parts/googleanalytics.php' );
-    echo "\n";
-  }
-  add_action('wp_footer', 'add_googleanalytics');
-}
 ?>
