0cd6c4c8d7f4ee2a175d9cdf48b8867542314df7
jarelsc <jculley@wescompapers.com>
2016-05-24 12:06:16 -0700

    fix footer

--- wp-content/themes/realhomes/footer.php
+++ wp-content/themes/realhomes/footer.php
@@ -55,7 +55,7 @@
                             <img src="/wp-content/uploads/2016/04/bh_logo.png" alt="Bend Homes &amp; Real Estate">
                           </a>
                           <!-- <h3>Find your dream home in Central Oregon</h3> -->
-                          <p style="font-size: 18px; color: #fff; font-weight: 300; line-height: 140%;">Sign up today to receive special alerts, listings, and knowledge articles about real estate in Central Oregon.</p>
+                          <!--p style="font-size: 18px; color: #fff; font-weight: 300; line-height: 140%;">Sign up today to receive special alerts, listings, and knowledge articles about real estate in Central Oregon.</p-->
                           <?php get_template_part("bend-homes/template-parts/gform-signup"); ?>
                           <?php /*<p style="font-size: 18px; color: #fff; font-weight: 300; line-height: 140%;">Sign up today to receive special alerts, listings, and knowledge articles about real estate in Central Oregon.</p>
                           <?php echo do_shortcode('[mc4wp_form id="280784"]');*/ ?>
