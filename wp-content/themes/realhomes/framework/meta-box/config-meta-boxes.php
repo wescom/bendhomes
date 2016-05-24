6dee3ef402ddd44f6c4b6de6633deccc9338c854
jarelsc <jculley@wescompapers.com>
2016-05-24 16:10:23 -0700

    metabox

--- wp-content/themes/realhomes/framework/meta-box/config-meta-boxes.php
+++ wp-content/themes/realhomes/framework/meta-box/config-meta-boxes.php
@@ -412,9 +412,9 @@ if( !function_exists( 'inspiry_register_meta_boxes' ) ) {
                     'tab' => 'home-slider',
                 ),
                 array(
-                    'name' => __("Slider Image: {$file_base}", "framework"),
+                    'name' => "Slider Image: {$file_base}",
                     'id' => "{$prefix}slider_image",
-                    'desc' => __('The recommended image size is 2000px by 700px. You can use bigger or smaller image but try to keep the same height to width ratio and use the exactly same size images for all properties that will be added in slider. {$prefix}', 'framework'),
+                    'desc' => __('The recommended image size is 2000px by 700px. You can use bigger or smaller image but try to keep the same height to width ratio and use the exactly same size images for all properties that will be added in slider.', 'framework'),
                     'type' => 'image_advanced',
                     'max_file_uploads' => 1,
                     'columns' => 12,
