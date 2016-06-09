35766ea2b82914b955cfc540ccb3e46947e2af75
jarelsc <jculley@wescompapers.com>
2016-06-09 10:27:39 -0700

    edit css loading for settings

--- wp-content/plugins/tbb-functions/admin/settings-company.php
+++ wp-content/plugins/tbb-functions/admin/settings-company.php
@@ -1,40 +1,6 @@
 <?php
 // Settings page under Company Post Type
 
-/*add_action( 'admin_init', 'company_options_init' );
-add_action( 'admin_menu', 'company_options_add_page' );
-
-function company_options_init() {
-	register_setting( 'company_settings', 'company_page_options');
-}
-
-function company_options_add_page() {
-	$company_page = add_submenu_page(
-        'edit.php?post_type=company',
-        __( 'Settings', 'tbb_company' ),
-        __( 'Settings', 'texttbb_companydomain' ),
-        'manage_options',
-        'company-settings',
-        'company_page_options_do_page'
-    );
-	add_action( 'load-' . $landing_page, 'load_company_files' );
-}
-
-function load_company_files() {
-	add_action( 'admin_enqueue_scripts', 'enqueue_company_files' );
-}
-
-function enqueue_landing_files() {
-	wp_enqueue_style( 'landing-css', plugin_dir_url( __FILE__ ) . 'css/company.css' );
-	//wp_enqueue_media();
-	//wp_enqueue_script( 'landing-js', plugin_dir_url( __FILE__ ) . 'js/company-page.js', array('jquery'), 1.0, true );
-}
-
-function company_page_options_do_page() {
-	
-}*/
-
-
 class CompanySettingsPage {
 
 	function __construct() {
@@ -57,7 +23,7 @@ class CompanySettingsPage {
 	}
 	
 	function enqueue_company_files() {
-		wp_enqueue_style( 'company-css', TBB_FUNCTIONS_DIR . '/css/company-settings.css' );
+		wp_enqueue_style( 'company', TBB_FUNCTIONS_URL . '/css/company-settings.css' );
 	}
 
 	function  company_settings_do_page() {
