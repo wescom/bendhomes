a0f672924e5d81b18bebbb893726fc70e115cf12
jarelsc <jculley@wescompapers.com>
2016-06-09 10:56:25 -0700

    setup settings plugin page

--- wp-content/plugins/tbb-functions/admin/settings-company.php
+++ wp-content/plugins/tbb-functions/admin/settings-company.php
@@ -6,6 +6,7 @@ class CompanySettingsPage {
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_company_files' ) );
+		add_action( 'admin_action_companies_created', array( $this, 'companies_created_admin_action' ) );
 	}
 
 	function admin_menu() {
@@ -25,9 +26,56 @@ class CompanySettingsPage {
 	function enqueue_company_files() {
 		wp_enqueue_style( 'company', TBB_FUNCTIONS_URL . 'css/company-settings.css' );
 	}
+	
+	function companies_created_admin_action() {
+		// Do posting function here that creates the companies.
+		$this->create_company_posts();
+	
+		wp_redirect( $_SERVER['HTTP_REFERER'] .'&companies-created="true' );
+		print_r($_POST);
+		exit();
+	}
 
-	function  company_settings_do_page() {
-		echo 'This is the page content';
+	function company_settings_do_page() { ?>
+		
+        <div class="wrap tbb-company-page">
+        	<h1>Company Settings</h1>
+            
+            <?php if ( $_GET['companies-created'] == 'true' ) { ?>
+                <div class="updated">
+                    <h3>Companies Created</h3>
+                </div>
+            <?php } ?> 
+            
+            <div class="company-wrap">
+            
+                <form method="post" action="<?php echo admin_url( 'admin.php' ); ?>" enctype="multipart/form-data">
+                	<h2 class="nav-tab-wrapper" id="tbb-company-tabs">
+                    	<a class="nav-tab nav-tab-active" id="tbb-company-tab" href="#top#company">Companies</a>
+                        <a class="nav-tab" id="tbb-shortcodes-tab" href="#top#shortcodes">Shortcodes</a>
+                    </h2>
+                    
+                    <div id="company" class="tbb-tab active">
+                    	<h3>Companies Settings</h3>
+                        <p>Create companies imported from Agents meta fields. To create company posts click the button below.</p>
+                        <p>
+                        	<input type="hidden" name="action" value="companies_created" />
+                        	<input class="button-primary" type="submit" value="<?php _e( 'Create Companies', 'tbb_company' ); ?>" />
+                        </p>
+                    </div>
+                    
+                    <div id="shortcodes" class="tbb-tab">
+                    	<h3>Shortcodes</h3>
+                    </div>
+                </form>
+            
+            </div>
+        </div>
+        
+	<?php }
+	
+	function create_company_posts() {
+		
 	}
 }
 
