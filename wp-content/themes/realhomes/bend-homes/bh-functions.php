56b63e59f28e238432b24a560f3fa577fc114a5c
jcontreras28 <janellec@bendbroadband.com>
2016-06-15 15:09:47 -0700

    date

--- wp-content/themes/realhomes/bend-homes/bh-functions.php
+++ wp-content/themes/realhomes/bend-homes/bh-functions.php
@@ -134,7 +134,7 @@ if ( ! function_exists( 'properties_updated_timestamp' ) ) {
     } else {
       $pulldate = strtotime('-30 days'); //'-6 hours' '-1 days'
     }
-    $showdate = date('F j, Y g:ia', $pulldate - 60*60^7);
+    $showdate = date('F j, Y g:ia', $pulldate - 60*60*7);
     //$showdate->sub('6H')
 
     $datetime_now = new DateTime("now");
