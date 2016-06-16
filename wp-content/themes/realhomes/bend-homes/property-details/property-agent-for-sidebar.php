8528479a85b128f8f63b4ea516d8e1bfc05eee19
jarelsc <jculley@wescompapers.com>
2016-06-16 16:16:10 -0700

    agent test

--- wp-content/themes/realhomes/bend-homes/property-details/property-agent-for-sidebar.php
+++ wp-content/themes/realhomes/bend-homes/property-details/property-agent-for-sidebar.php
@@ -92,6 +92,7 @@ function bhAgentRender($page_position) {
 						while( $company_post->have_posts() ) : $company_post->the_post();
 							
 							$company_is_featured = get_field( 'company_featured_company' );
+							var_dump( $company_is_featured );
 							if($company_is_featured) {
 								$company_featured = 'yes';
 							} else {
