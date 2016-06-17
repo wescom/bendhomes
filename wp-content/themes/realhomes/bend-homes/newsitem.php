e8dcfa85e3d46ee1e3cf7b166252c9f6a022ad50
jarelsc <jculley@wescompapers.com>
2016-06-17 08:14:03 -0700

    recent post image placeholder urls

--- wp-content/themes/realhomes/bend-homes/newsitem.php
+++ wp-content/themes/realhomes/bend-homes/newsitem.php
@@ -25,7 +25,7 @@ if ( has_post_thumbnail() ){ ?>
 <?php } else { ?>
   
 	<figure>
-    	<img src="<?php echo get_template_directory_uri(); ?>/images/bh-placeholder.jpg" alt="" />
+    	<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/bh-placeholder.jpg" alt="" />
     </figure>
   
 <?php } ?>
