0ff7bb1e89671d70ab73e78f113d4b4ea2e0b6a6
jarelsc <jculley@wescompapers.com>
2016-06-27 13:53:38 -0700

    pagination test, no ajax

--- wp-content/themes/realhomes/framework/functions/pagination.php
+++ wp-content/themes/realhomes/framework/functions/pagination.php
@@ -26,6 +26,7 @@ if ( ! function_exists( 'theme_pagination' ) ) {
 		$next = $paged + 1;
 		$range = 2; // only change it to show more links
 		$show_items = ( $range * 2 ) + 1;
+		$jump_link = 'home-properties-section';
 
 		if ( $pages == '' ) {
 			global $wp_query;
@@ -37,17 +38,17 @@ if ( ! function_exists( 'theme_pagination' ) ) {
 
 		if ( 1 != $pages ) {
 			echo "<div class='pagination'>";
-			echo ( $paged > 2 && $paged > $range + 1 && $show_items < $pages ) ? "<a href='" . get_pagenum_link( 1 ) . "' class='real-btn'>&laquo; " . __( 'First', 'framework' ) . "</a> " : "";
-			echo ( $paged > 1 && $show_items < $pages ) ? "<a href='" . get_pagenum_link( $prev ) . "' class='real-btn' >&laquo; " . __( 'Previous', 'framework' ) . "</a> " : "";
+			echo ( $paged > 2 && $paged > $range + 1 && $show_items < $pages ) ? "<a href='" . get_pagenum_link( 1 ) . "#". $jump_link ."' class='real-btn'>&laquo; " . __( 'First', 'framework' ) . "</a> " : "";
+			echo ( $paged > 1 && $show_items < $pages ) ? "<a href='" . get_pagenum_link( $prev ) . "#". $jump_link ."' class='real-btn' >&laquo; " . __( 'Previous', 'framework' ) . "</a> " : "";
 
 			for ( $i = 1; $i <= $pages; $i++ ) {
 				if ( 1 != $pages && ( ! ( $i >= $paged + $range + 1 || $i <= $paged - $range - 1 ) || $pages <= $show_items ) ) {
-					echo ( $paged == $i ) ? "<a href='" . get_pagenum_link( $i ) . "' class='real-btn current' >" . $i . "</a> " : "<a href='" . get_pagenum_link( $i ) . "' class='real-btn'>" . $i . "</a> ";
+					echo ( $paged == $i ) ? "<a href='" . get_pagenum_link( $i ) . "#". $jump_link ."' class='real-btn current' >" . $i . "</a> " : "<a href='" . get_pagenum_link( $i ) . "' class='real-btn'>" . $i . "</a> ";
 				}
 			}
 
-			echo ( $paged < $pages && $show_items < $pages ) ? "<a href='" . get_pagenum_link( $next ) . "' class='real-btn' >" . __( 'Next', 'framework' ) . " &raquo;</a> " : "";
-			echo ( $paged < $pages - 1 && $paged + $range - 1 < $pages && $show_items < $pages ) ? "<a href='" . get_pagenum_link( $pages ) . "' class='real-btn' >" . __( 'Last', 'framework' ) . " &raquo;</a> " : "";
+			echo ( $paged < $pages && $show_items < $pages ) ? "<a href='" . get_pagenum_link( $next ) . "#". $jump_link ."' class='real-btn' >" . __( 'Next', 'framework' ) . " &raquo;</a> " : "";
+			echo ( $paged < $pages - 1 && $paged + $range - 1 < $pages && $show_items < $pages ) ? "<a href='" . get_pagenum_link( $pages ) . "#". $jump_link ."' class='real-btn' >" . __( 'Last', 'framework' ) . " &raquo;</a> " : "";
 			echo "</div>";
 		}
 	}
