5ffa6fda5c98d00007a9fddc9e1a027aaed13593
jarelsc <jculley@wescompapers.com>
2016-06-30 12:26:13 -0700

    mail chimp form edits

--- wp-content/themes/realhomes/bend-homes/template-parts/mailchimp_embed_form.php
+++ wp-content/themes/realhomes/bend-homes/template-parts/mailchimp_embed_form.php
@@ -4,15 +4,15 @@
 <form action="//bendbulletin.us1.list-manage.com/subscribe/post?u=a5d36976165603b3ce7485798&amp;id=5e8299d4c6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
     <div id="mc_embed_signup_scroll">
 	
-<div class="mc-field-group">
+<div class="mc-field-group mc-email">
 	<label for="mce-EMAIL">Email Address </label>
 	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
 </div>
-<div class="mc-field-group">
+<div class="mc-field-group mc-first">
 	<label for="mce-FNAME">First Name </label>
 	<input type="text" value="" name="FNAME" class="" id="mce-FNAME">
 </div>
-<div class="mc-field-group">
+<div class="mc-field-group mc-last">
 	<label for="mce-LNAME">Last Name </label>
 	<input type="text" value="" name="LNAME" class="" id="mce-LNAME">
 </div>
@@ -21,7 +21,7 @@
 		<div class="response" id="mce-success-response" style="display:none"></div>
 	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_a5d36976165603b3ce7485798_5e8299d4c6" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <div class="clear mc-button"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="real-btn btn"></div>
     </div>
 </form>
 </div>
