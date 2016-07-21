54339f8daf0042fbdf211620c320ab4f589b5470
jarelsc <jculley@wescompapers.com>
2016-07-21 14:54:28 -0700

    settings

--- wp-content/plugins/tbb-functions/admin/settings-company.php
+++ wp-content/plugins/tbb-functions/admin/settings-company.php
@@ -1,44 +1,103 @@
 <?php
-class Test_Options {
+/*class Test_Options {
 
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'add_plugin_settings_menu' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_company_files' ) );
 		add_action( 'admin_init', array( $this, 'register_settings' ) );
+	}
+	
+	function enqueue_company_files() {
+		wp_enqueue_style( 'company', TBB_FUNCTIONS_URL . 'css/company-settings.css' );
 	}
 
 	function add_plugin_settings_menu() {
 		// add_options_page( $page_title, $menu_title, $capability, $menu_slug, $function )
-		add_options_page( 'Test Plugin', 'Test', 'manage_options', 'test-plugin', array($this, 'create_plugin_settings_page') );
+		//add_options_page( 'Test Plugin', 'Test', 'manage_options', 'test-plugin', array($this, 'create_plugin_settings_page') );
+		add_submenu_page(
+			'edit.php?post_type=company',
+			'Company Settings',
+			'Settings',
+			'manage_options',
+			'company-settings',
+			array( $this, 'create_plugin_settings_page' )
+		);
 	}
 
 	function create_plugin_settings_page() {
 	?>
-<div class="wrap">
-	<?php screen_icon(); ?>
-    <h2>Settings</h2>
+    
+<div class="wrap tbb-company-page">
+	<h1>Company Settings</h1>
+    
+    <div class="company-wrap">
+            
+        <h2 class="nav-tab-wrapper" id="tbb-company-tabs">
+            <a class="nav-tab nav-tab-active" id="tbb-general-tab" href="#top#general">General</a>
+            <a class="nav-tab" id="tbb-shortcodes-tab" href="#top#shortcodes">Shortcodes</a>
+        </h2>
+        
+        <div id="sections">
+            <section id="general" class="tbb-tab active">
+                <h3>General Settings</h3>
+                <form id="create-companies" method="post" action="<?php echo admin_url( 'admin.php' ); ?>" enctype="multipart/form-data">
+				<?php
+                    // This prints out all hidden setting fields
+                    // settings_fields( $option_group )
+                    settings_fields( 'main-settings-group' );
+                    // do_settings_sections( $page )
+                    do_settings_sections( 'test-plugin-main-settings-section' );
+                ?>
+                <?php submit_button('Save Changes'); ?>
+                </form>
+                
+                <form id="setup-featured-agents" method="post" action="<?php echo admin_url( 'admin.php' ); ?>" enctype="multipart/form-data">
+				<?php
+                    // This prints out all hidden setting fields
+                    // settings_fields( $option_group )
+                    settings_fields( 'additional-settings-group' );
+                    // do_settings_sections( $page )	
+                    do_settings_sections( 'test-plugin-additional-settings-section' );
+                ?>
+                <?php submit_button('Save Changes'); ?>
+                </form>
+            </section>
+            
+            <section id="shortcodes" class="tbb-tab">
+                <h3>Shortcodes</h3>
+                <p>This section displays a list of available shortcodes to use with the Company post type to display companies on the website.</p>
+                <p><strong>Shortcode: [BH_CUSTOM_POSTS]</strong></p>
+            </section>
+        </div>
+    
+    </div>
+</div><!-- end wrap -->
+<?php /* Unminified JS
+<script type="text/javascript">
+	(function($) {
+		$(document).on( 'click', '.nav-tab-wrapper a', function() {
+			$('section').hide().removeClass('active');
+			$('.nav-tab-wrapper a').removeClass('nav-tab-active');
+			$('.nav-tab-wrapper a').eq($(this).index()).addClass('nav-tab-active');
+			$('section').eq($(this).index()).show().addClass('active');
+			return false;
+		})
+		$('#company-submit').click(function(e) {	
+			e.preventDefault();
+			$(this).attr("disabled","disabled");
+			if( confirm("If you're sure, click OK to continue. This will take several minutes.") ) {
+				$("#create-companies").submit();
+				$("#company-submit").after('<span class="holdon">Please hold, we\'re creating your companies.</span>');
+			} else {
+				$(this).removeAttr("disabled");	
+			}
+		});
+	})( jQuery );
+</script> */ /* ?>
+<script type="text/javascript">
+!function(a){a(document).on("click",".nav-tab-wrapper a",function(){return a("section").hide().removeClass("active"),a(".nav-tab-wrapper a").removeClass("nav-tab-active"),a(".nav-tab-wrapper a").eq(a(this).index()).addClass("nav-tab-active"),a("section").eq(a(this).index()).show().addClass("active"),!1}),a("#company-submit").click(function(e){e.preventDefault(),a(this).attr("disabled","disabled"),confirm("If you're sure, click OK to continue. This may take a few minutes.")?(a("#create-companies").submit(),a("#company-submit").after('<span class="holdon">Please hold on, we\'re creating/updating all companies.</span>')):a(this).removeAttr("disabled")})}(jQuery);
+</script>
 
-    <form method="post" action="options.php">
-    <?php
-		// This prints out all hidden setting fields
-		// settings_fields( $option_group )
-		settings_fields( 'main-settings-group' );
-		// do_settings_sections( $page )
-		do_settings_sections( 'test-plugin-main-settings-section' );
-    ?>
-    <?php submit_button('Save Changes'); ?>
-    </form>
-
-    <form method="post" action="options.php">
-    <?php
-		// This prints out all hidden setting fields
-		// settings_fields( $option_group )
-		settings_fields( 'additional-settings-group' );
-		// do_settings_sections( $page )	
-		do_settings_sections( 'test-plugin-additional-settings-section' );
-    ?>
-    <?php submit_button('Save Changes'); ?>
-    </form>
-</div>
 	<?php
 	}
 
@@ -47,7 +106,7 @@ class Test_Options {
 		// add_settings_section( $id, $title, $callback, $page )
 		add_settings_section(
 			'main-settings-section',
-			'Main Settings',
+			'Create/Update Companies',
 			array($this, 'print_main_settings_section_info'),
 			'test-plugin-main-settings-section'
 		);
@@ -55,7 +114,7 @@ class Test_Options {
 		// add_settings_field( $id, $title, $callback, $page, $section, $args )
 		add_settings_field(
 			'some-setting', 
-			'Some Setting', 
+			'Create Featured Agents From Companies', 
 			array($this, 'create_input_some_setting'), 
 			'test-plugin-main-settings-section', 
 			'main-settings-section'
@@ -86,7 +145,7 @@ class Test_Options {
 	}
 
 	function print_main_settings_section_info() {
-		echo '<p>Main Settings Description.</p>';
+		echo '<p>Create companies imported from Agents meta fields. To create company posts click the button below.</p>';
 	}
 
 	function create_input_some_setting() {
@@ -101,7 +160,7 @@ class Test_Options {
 	}
 
 	function print_additional_settings_section_info() {
-		echo '<p>Additional Settings Description.</p>';
+		echo '<p>This button sets the "Company is Featured" checkbox for all agents whose company is checked as "Featured". </p>';
 	}
 
 	function create_input_another_setting() {
@@ -117,7 +176,7 @@ class Test_Options {
 
 }
 
-$test_options = new Test_Options;
+$companySettings = new CompanySettingsPage;*/
 
 
 
@@ -270,6 +329,7 @@ class CompanySettingsPage {
 				
 					$company_check = get_page_by_title($company_name, 'OBJECT', 'company');
 					$agents_list = get_post_meta( $company_check->ID, 'company_agents', true );
+					$company_is_featured = get_post_meta( $company_check->ID, 'company_featured_company', true );
 					if( !array( $agents_list ) ) $agents_list = array();
 					if( !in_array( $agent_id, $agents_list ) ) $agents_list[] = $agent_id;
 					//$final_list = array_values( array_unique( $agents_list ) );
@@ -278,7 +338,15 @@ class CompanySettingsPage {
 					update_post_meta($company_check->ID, 'company_office_address', str_replace('<br />', '', $company_address) );
 					
 					update_post_meta( $company_check->ID, 'company_agents', $agents_list );
-					
+											
+					foreach( $agents_list as $agent_item ) {
+						query_posts( 'p='. $agent_item );
+						while (have_posts()) : the_post();
+							update_post_meta( get_the_ID(), 'brk_office_is_featured', $company_is_featured );
+						endwhile;
+						wp_reset_query();
+					}
+											
 				}
 			
 			endwhile;
@@ -289,7 +357,7 @@ class CompanySettingsPage {
 		wp_reset_query();
 	}
 	
-	function feature_agents_from_company() {
+	/*function feature_agents_from_company() {
 		$company_args = array(
 			'post_type' => 'company',
 			'posts_per_page' => '-1'
@@ -334,7 +402,7 @@ class CompanySettingsPage {
 		return;
 		
 		wp_reset_query();
-	}
+	}*/
 	
 }
 
