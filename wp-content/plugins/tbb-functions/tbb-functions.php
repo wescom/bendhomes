051f58fc31dcd1b83f96389881cff878953e5360
jarelsc <jculley@wescompapers.com>
2016-07-26 13:19:18 -0700

    search

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -169,15 +169,12 @@ function populate_agent_admin_column($column_name, $term_id) {
 }
 
 
-/**
- * Search SQL filter for matching against post title only.
- */
-function __search_by_title_only( $search, &$wp_query )
-{
-     /*my solution */
+// Filter to only search Agents by name, ie post_title.
+add_filter( 'posts_search', 'tbb_search_by_title_only', 500, 2 );
+function tbb_search_by_title_only( $search, &$wp_query ) {
+
      if($_GET['post_type'] != 'agent' )
         return $search;
-     /*my solution*/
 
 	if ( ! empty( $search ) && ! empty( $wp_query->query_vars['search_terms'] ) ) {
         global $wpdb;
@@ -190,7 +187,7 @@ function __search_by_title_only( $search, &$wp_query )
         foreach ( ( array ) $q['search_terms'] as $term )
             $search[] = $wpdb->prepare( "$wpdb->posts.post_title LIKE %s", $n . $wpdb->esc_like( $term ) . $n );
 
-        if ( ! is_user_logged_in() )
+        if ( !is_user_logged_in() )
             $search[] = "$wpdb->posts.post_password = ''";
 
         $search = ' AND ' . implode( ' AND ', $search );
@@ -198,4 +195,3 @@ function __search_by_title_only( $search, &$wp_query )
 
     return $search;
 }
-add_filter( 'posts_search', '__search_by_title_only', 500, 2 );
