df13394dad615a57c9ea57b61df4d790b936a07e
jarelsc <jculley@wescompapers.com>
2016-08-10 14:07:20 -0700

    blog layout

--- wp-content/themes/realhomes/template-parts/article-for-listing.php
+++ wp-content/themes/realhomes/template-parts/article-for-listing.php
@@ -7,18 +7,18 @@
 ?>
 
 <article <?php post_class( array('row-fluid') ); ?>>
-	<div class="span4">
+	<div class="span5">
     	<?php get_template_part( "post-formats/$format" ); ?>
     </div>
-    <div class="span8">
+    <div class="span7">
     	<header>
             <h3 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <div class="post-meta <?php echo $format; ?>-meta thumb-<?php echo has_post_thumbnail()?'exist':'not-exist'; ?>">
+            <small class="post-meta <?php echo $format; ?>-meta thumb-<?php echo has_post_thumbnail()?'exist':'not-exist'; ?>">
                 <span><?php _e('Posted on', 'framework'); ?> <span class="date"> <?php the_time('F d, Y'); ?></span></span>
                 <span><?php _e('by', 'framework'); ?> <span class="author-link"><?php the_author() ?></span> <?php _e('in', 'framework'); ?> <?php the_category(', '); ?> </span>
-            </div>
+            </small>
         </header>
-        <p><?php framework_excerpt(40);  ?></p>
-    	<a class="real-btn" href="<?php the_permalink(); ?>"><?php _e('Read more', 'framework'); ?></a>
+        <p><?php framework_excerpt(30);  ?></p>
+    	<?php /*<a class="real-btn" href="<?php the_permalink(); ?>"><?php _e('Read more', 'framework'); ?></a>*/ ?>
     </div>
 </article>
