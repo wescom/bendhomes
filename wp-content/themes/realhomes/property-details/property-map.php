fbd2a683518eef31140f5a03abe94437c8fc1d65
jarelsc <jculley@wescompapers.com>
2016-08-15 09:51:07 -0700

    maps script

--- wp-content/themes/realhomes/property-details/property-map.php
+++ wp-content/themes/realhomes/property-details/property-map.php
@@ -2,66 +2,13 @@
 $display_google_map = get_option('theme_display_google_map');
 //$display_social_share = get_option('theme_display_social_share');
 //if($display_google_map == 'true' || $display_social_share == 'true'){
-if($display_google_map == 'true') { ?>
+if($display_google_map == 'true') {
+	global $post;
+	
+	$property_address = get_post_meta($post->ID,'REAL_HOMES_property_address',true);
+	?>
 
     <div class="map-wrap clearfix">
-        
-		<?php
-        add_action('custom_footer_scripts', 'load_maps_script_in_footer');
-        function load_maps_script_in_footer() {
-			global $post;
-            $property_location = get_post_meta($post->ID,'REAL_HOMES_property_location',true);
-            // set a trap if we get zeroes for map lat long from MLS | 1777 JTG
-            if($property_location == '0.000000,0.000000') {
-              unset($property_location);
-            }
-            $property_address = get_post_meta($post->ID,'REAL_HOMES_property_address',true);
-            $property_map = get_post_meta($post->ID,'REAL_HOMES_property_map',true);
-        
-            if( $property_address && !empty($property_location) && $display_google_map == 'true' && ( $property_map != 1 ) ) {
-                $property_marker = array();
-                $lat_lng = explode(',',$property_location);
-                $property_marker['lat'] = $lat_lng[0];
-                $property_marker['lang'] = $lat_lng[1];
-        
-                /* Property Map Icon Based on Property Type */
-                $property_type_slug = 'single-family-home'; // Default Icon Slug
-        
-                $type_terms = get_the_terms( $post->ID,"property-type" );
-                if( !empty( $type_terms ) ) {
-                    foreach($type_terms as $typ_trm){
-                        $property_type_slug = $typ_trm->slug;
-                        break;
-                    }
-                }
-        
-                if( file_exists( get_template_directory().'/images/map/'.$property_type_slug.'-map-icon.png' ) ){
-                    $property_marker['icon'] = get_template_directory_uri().'/images/map/'.$property_type_slug.'-map-icon.png';
-                    // retina icon
-                    if( file_exists( get_template_directory().'/images/map/'.$property_type_slug.'-map-icon@2x.png' ) ) {
-                        $property_marker['retinaIcon'] = get_template_directory_uri().'/images/map/'.$property_type_slug.'-map-icon@2x.png';
-                    }
-                }else{
-                    $property_marker['icon'] = get_template_directory_uri().'/images/map/single-family-home-map-icon.png';// default icon
-                    $property_marker['retinaIcon'] = get_template_directory_uri().'/images/map/single-family-home-map-icon@2x.png';  // default retina icon
-                }
-        
-        
-                $property_map_title = get_option('theme_property_map_title');
-                if( !empty($property_map_title) ){
-                    ?><span class="map-label"><?php echo $property_map_title; ?></span><?php
-                }
-                ob_start(); ?>
-                
-                <script type="application/javascript">
-                function initialize_property_map(){var e=<?php echo json_encode( $property_marker ); ?>,o=e.icon,n=new google.maps.Size(42,57);window.devicePixelRatio>1.5&&e.retinaIcon&&(o=e.retinaIcon,n=new google.maps.Size(83,113));var a={url:o,size:n,scaledSize:new google.maps.Size(42,57),origin:new google.maps.Point(0,0),anchor:new google.maps.Point(21,56)},i=new google.maps.LatLng(e.lat,e.lang),p={center:i,zoom:15,mapTypeId:google.maps.MapTypeId.ROADMAP,scrollwheel:!1},g=new google.maps.Map(document.getElementById("property_map"),p);new google.maps.Marker({position:i,map:g,icon:a})}window.onload=initialize_property_map();
-                </script>
-                
-            <?php
-            }
-            
-            echo ob_get_clean(); 
-        } // end function ?>
         
         <div id="property_map"></div>
         
