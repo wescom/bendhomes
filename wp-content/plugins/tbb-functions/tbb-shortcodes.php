f1a9b8179da434a12e1d7f5ddfa5ca009f1d67b1
jarelsc <jculley@wescompapers.com>
2016-08-15 11:56:59 -0700

    agent placeholders

--- wp-content/plugins/tbb-functions/tbb-shortcodes.php
+++ wp-content/plugins/tbb-functions/tbb-shortcodes.php
@@ -587,10 +587,8 @@ function tbb_display_agents( $defaults ) {
 			$output .= sprintf( '<div class="custom-post custom-post-%s %s %s %s %s"><div class="custom-post-item clearfix">', 
 							$count, $cols, $classes, $has_image_class, $category_classes );
 			
-				if( !empty( $image ) ) {
-					$output .= sprintf( '<figure class="custom-post-image image-agent-image %s"><a href="%s"><img src="%s" width="%s" height="%s" /></a></figure>', 
-									$count, $permalink, $image_url, $image[1], $image[2] );
-				}
+				$output .= sprintf( '<figure class="custom-post-image image-agent-image %s"><a href="%s"><img src="%s" width="%s" height="%s" /></a></figure>', 
+								$count, $permalink, $image_url, $image_width, $image_height );
 								
 				$output .= sprintf( '<h4 class="custom-post-title"><a href="%s">%s</a></h4>', 
 								$permalink, $title );
