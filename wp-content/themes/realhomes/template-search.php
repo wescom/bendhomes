6801ce93b5dfc4ae2cf3aadbecbe1ce381f5aa07
jarelsc <jculley@wescompapers.com>
2016-08-18 15:53:46 -0700

    map list

--- wp-content/themes/realhomes/template-search.php
+++ wp-content/themes/realhomes/template-search.php
@@ -35,8 +35,6 @@ $total_count = $search_query->found_posts;
 
 $text = $total_count == 1 ? 'Search Result' : 'Search Results';
 
-$current_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-
 if(isset($_GET['view'])){
 	$view_type = $_GET['view'];
 }else{
@@ -65,13 +63,13 @@ if(isset($_GET['view'])){
                             <div class="search-header clearfix">
                             
                             	<div class="view-type clearfix">
-                                	<a class="map <?php echo ( $view_type == 'map' )?'active':''; ?>" href="<?php echo $current_link . '&view=map'; ?>">
+                                	<a class="map <?php echo ( $view_type == 'map' )?'active':''; ?>" href="<?php echo tbb_current_url( 'view=map' ); ?>">
                                         <i class="fa fa-map-marker"></i>
                                     </a>
-                                    <a class="list <?php echo ( $view_type == 'list' )?'active':''; ?>" href="<?php echo $current_link '&view=list'; ?>">
+                                    <a class="list <?php echo ( $view_type == 'list' )?'active':''; ?>" href="<?php echo tbb_current_url( 'view=list' ); ?>">
                                         <i class="fa fa-list"></i>
                                     </a>
-                                    <a class="grid <?php echo ( $view_type == 'grid' )?'active':''; ?>" href="<?php echo $current_link . '&view=grid'; ?>">
+                                    <a class="grid <?php echo ( $view_type == 'grid' )?'active':''; ?>" href="<?php echo tbb_current_url( 'view=grid' ); ?>">
                                         <i class="fa fa-th"></i>
                                     </a>
                                 </div>
