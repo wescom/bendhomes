2a9a6a1f1dae270c452e21c3e324b15f3d69f508
jarelsc <jculley@wescompapers.com>
2016-08-18 11:39:13 -0700

    testing map modal

--- wp-content/themes/realhomes/bend-homes/template-parts/map-modal.php
+++ wp-content/themes/realhomes/bend-homes/template-parts/map-modal.php
@@ -1,3 +1,15 @@
 <?php // Map Modal ?>
 
-<div class="test-class-here"></div>
+<!-- Map Modal -->
+<div id="map-modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="mapModal" aria-hidden="true">
+	<div class="modal-scrollable">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+        </div>
+        <div class="modal-body">
+            
+            <p>Test Map Modal Content</p>
+            
+        </div>
+    </div>
+</div>
