ac301b9b9087eaf109a68fcaa2481fa8b7c36dc2
jarelsc <jculley@wescompapers.com>
2016-08-23 10:01:34 -0700

    fallback image for banner

--- wp-content/themes/realhomes/template-parts/slider.php
+++ wp-content/themes/realhomes/template-parts/slider.php
@@ -37,7 +37,10 @@ if ( $slider_query->have_posts() ) { ?>
 				$slider_query->the_post();
 				
 				$image_id = get_post_meta( $post->ID, 'REAL_HOMES_slider_image', true );
+				$image_parts = pathinfo( $image_id[0] );
+				if( $image_parts['filename'] == 'default' ) $image_id = '';				
 				if( !$image_id ) $image_id = get_post_thumbnail_id();
+				
 				$slider_image = wp_get_attachment_image_src( $image_id, 'large', true);
 				?>
                 
