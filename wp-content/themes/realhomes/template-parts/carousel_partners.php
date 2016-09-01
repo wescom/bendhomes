c2b014b9f81243b77098f06503ba2acda147c304
jcontreras28 <janellec@bendbroadband.com>
2016-09-01 10:49:31 -0700

    changed span

--- wp-content/themes/realhomes/template-parts/carousel_partners.php
+++ wp-content/themes/realhomes/template-parts/carousel_partners.php
@@ -10,15 +10,15 @@ if($show_partners == 'true'){
                     <h3><span><?php echo $partners_title = get_option('theme_partners_title'); ?></span></h3>
                             <div class="row-fluid clearfix">
                                 
-                                <div class="span3 partner"><?php do_action('dfp_ad_spot','partners1'); ?></div>
+                                <div class="span2 partner"><?php do_action('dfp_ad_spot','partners1'); ?></div>
                                 
-                                <div class="span3 partner"><?php do_action('dfp_ad_spot','partners2'); ?></div>
+                                <div class="span2 partner"><?php do_action('dfp_ad_spot','partners2'); ?></div>
                                 
-                                <div class="span3 partner"><?php do_action('dfp_ad_spot','partners3'); ?></div>
+                                <div class="span2 partner"><?php do_action('dfp_ad_spot','partners3'); ?></div>
                                 
-                                <div class="span3 partner"><?php do_action('dfp_ad_spot','partners4'); ?></div>
+                                <div class="span2 partner"><?php do_action('dfp_ad_spot','partners4'); ?></div>
                                 
-                                <div class="span3 partner"><?php do_action('dfp_ad_spot','partners5'); ?></div>
+                                <div class="span2 partner"><?php do_action('dfp_ad_spot','partners5'); ?></div>
                                 
                             </div>
                 </section>
