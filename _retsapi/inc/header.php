c3426183dcf18632cee9f4ce03253b4fd7fa7ee9
jcontreras28 <janellec@bendbroadband.com>
2016-09-02 17:14:31 -0700

    jc

--- _retsapi/inc/header.php
+++ _retsapi/inc/header.php
@@ -40,7 +40,7 @@ function universalqueries($pulltime) {
       'LAND' => '(ListingRid=0+), (LastModifiedDateTime='.$pulltime.'+)',
       'MULT' => '(ListingRid=0+), (LastModifiedDateTime='.$pulltime.'+)',
       //'RESI' => '(ListingRid=0+), (LastModifiedDateTime='.$pulltime.'+)'
-	    'RESI' => '(MLNumber=201600659)'
+	    'RESI' => '(MLNumber=201601958)'
     ),
     'ActiveAgent' => array(
       'MEMB' => '(MemberNumber=0+), (LastModifiedDateTime='.$pulltime.'+)',
