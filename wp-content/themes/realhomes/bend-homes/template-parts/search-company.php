a6dcf1c268ce829a27ff7a5acf7aeb4db260a95f
jarelsc <jculley@wescompapers.com>
2016-09-20 14:19:26 -0700

    more image size fixing

--- wp-content/themes/realhomes/bend-homes/template-parts/search-company.php
+++ wp-content/themes/realhomes/bend-homes/template-parts/search-company.php
@@ -6,7 +6,7 @@ $id = get_the_ID();
 $permalink = get_permalink();
 $title = get_the_title();
 		
-$image_size = 'medium';
+$image_size = 'property-thumb-image';
 $phone = get_field( 'company_office_phone' );
 $fax = get_field( 'company_office_fax' );
 $address = get_field( 'company_office_address' );
@@ -29,27 +29,25 @@ $has_image_class = !empty( $image ) ? 'with-image' : '';
 
 
 
-	<?php
-	// Begin item output
-	$output = sprintf( '<article class="custom-post one %s %s %s"><div class="custom-post-item clearfix">', 
-					$classes, $has_image_class, $category_classes );
-	
-		if( !empty( $image ) ) {
-			$output .= sprintf( '<figure class="custom-post-image %s"><a href="%s"><img src="%s" width="%s" height="%s" /></a></figure>', 
-							$image_size, $permalink, $image[0], $image[1], $image[2] );
-		}
-				
-		$output .= sprintf( '<h4 class="custom-post-title"><a href="%s">%s</a></h4>', 
-						$permalink, $title );
-		
-		$output .= $additional_meta;
-		
-		$output .= sprintf( '<a class="more-details" href="%s">More Details <i class="fa fa-caret-right"></i></a>', 
-						$permalink );
-	
-	$output .= '</div></article>';
-	// End item ouput
-	
-	echo $output;
-	?>
+<?php
+// Begin item output
+$output = sprintf( '<article class="custom-post one %s %s %s"><div class="custom-post-item clearfix">', 
+				$classes, $has_image_class, $category_classes );
 
+	if( !empty( $image ) ) {
+		$output .= sprintf( '<figure class="custom-post-image %s"><a href="%s"><img src="%s" width="%s" height="%s" /></a></figure>', 
+						$image_size, $permalink, $image[0], $image[1], $image[2] );
+	}
+			
+	$output .= sprintf( '<h4 class="custom-post-title"><a href="%s">%s</a></h4>', 
+					$permalink, $title );
+	
+	$output .= $additional_meta;
+	
+	$output .= sprintf( '<a class="more-details" href="%s">More Details <i class="fa fa-caret-right"></i></a>', 
+					$permalink );
+
+$output .= '</div></article>';
+// End item ouput
+
+echo $output;
