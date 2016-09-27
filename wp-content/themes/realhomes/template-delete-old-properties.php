7faa25f10cbbcd4d640f4f46b74b3d3a56820d8d
jcontreras28 <janellec@bendbroadband.com>
2016-09-27 15:08:47 -0700

    delete year old solds template work

--- wp-content/themes/realhomes/template-delete-old-properties.php
+++ wp-content/themes/realhomes/template-delete-old-properties.php
@@ -8,11 +8,18 @@
 
 ini_set('max_execution_time', 0);
 date_default_timezone_set('America/Los_Angeles');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 /* #### INCLUDES ##### */
 include_once ABSPATH . 'wp-admin/includes/media.php';
 include_once ABSPATH . 'wp-admin/includes/file.php';
 include_once ABSPATH . 'wp-admin/includes/image.php';
+include_once WP_PLUGIN_DIR . '/'.'bh-importer/functions.php';
+
+$theTm = time();
+//bh_write_to_log('Entered template-import-properties.php ','propertiesUpdateEntry'.$theTm."_".$_SERVER['REMOTE_ADDR']);
 
 echo "hello world";
 
@@ -26,31 +33,7 @@ echo "hello world";
 
   $mysqli = new mysqli($db['host'], $db['username'], $db['password'], $db['database']);
   /* check connection */
-  if ($mysqli->connect_errno) {
-  	  echo "connect failed!"
-      printf("Connect failed: %s\n", $mysqli->connect_error);
-      exit();
-  }
-
-  $querydate = date('Y-m-d H:i:s -1 year');
-  // echo $pulldate;
-  /* AND images IS NOT NULL */
-  // AND Status = 'Active'
-
-  $sqlquery = "SELECT * FROM Property_RESI WHERE
-              PublishToInternet = 1
-              AND lastUpdateTime <= '".$querydate."'
-              AND Status = 'Sold'";
-
-  echo "\n\rquery: ".$sqlquery."\n\r";
-  /*if ($result = $mysqli->query($sqlquery)) {
-      // printf("Select returned %d rows.\n", $result->num_rows);
-      while($row = $result->fetch_assoc()) {
-          $data[] = $row;
-      }
-      // Frees the memory associated with a result
-      $result->free();
-  }*/
+  
 
   $mysqli->close();
 
