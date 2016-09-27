f9022b26fa7c8c3fbc11de1ea8fcb90bd5f06f6a
jcontreras28 <janellec@bendbroadband.com>
2016-09-27 14:19:16 -0700

    Template to delete old sold properties

---
+++ wp-content/themes/realhomes/template-delete-old-properties.php
@@ -0,0 +1,19 @@
+<?php
+/*
+*  Template Name: Delete Old Properties Template
+*/
+/*
+*  Author: Janelle Contreras
+*/
+
+ini_set('max_execution_time', 0);
+date_default_timezone_set('America/Los_Angeles');
+
+/* #### INCLUDES ##### */
+include_once ABSPATH . 'wp-admin/includes/media.php';
+include_once ABSPATH . 'wp-admin/includes/file.php';
+include_once ABSPATH . 'wp-admin/includes/image.php';
+
+echo "hello world";
+
+?>
