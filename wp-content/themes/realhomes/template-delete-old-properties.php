bf4ada92af51c461764048dac64f33f61f922be4
jcontreras28 <janellec@bendbroadband.com>
2016-09-27 16:11:00 -0700

    delete year old solds template work

--- wp-content/themes/realhomes/template-delete-old-properties.php
+++ wp-content/themes/realhomes/template-delete-old-properties.php
@@ -28,7 +28,8 @@ echo "hello world";
 
 foreach($propList as $propItem) {
  	$mlsposts = bhLookupPostByMLS($propItem['MLNumber']);
-    echo "<p>mls: ".$propItem['MLNumber']."wpID: ".$mlsposts." status: ".$propItem['Status']." lastMod: ".$propItem["LastModifiedDateTime"]."</p>";
+ 	$bhpropertyid = $mlsposts[0];
+    echo "<p>mls: ".$propItem['MLNumber']."wpID: ".$bhpropertyid." status: ".$propItem['Status']." lastMod: ".$propItem["LastModifiedDateTime"]."</p>";
 }
 
 ?>
