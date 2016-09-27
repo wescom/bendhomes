ca43a8084ca846154320a6c00c02125084dd4abc
jcontreras28 <janellec@bendbroadband.com>
2016-09-27 15:10:40 -0700

    delete year old solds template work

--- wp-content/themes/realhomes/template-delete-old-properties.php
+++ wp-content/themes/realhomes/template-delete-old-properties.php
@@ -40,17 +40,7 @@ echo "hello world";
       exit();
   }
 
-  $querydate = date('Y-m-d H:i:s -1 year');
-  // echo $pulldate;
-  /* AND images IS NOT NULL */
-  // AND Status = 'Active'
-
-  $sqlquery = "SELECT * FROM Property_RESI WHERE
-              PublishToInternet = 1
-              AND lastUpdateTime <= '".$querydate."'
-              AND Status = 'Sold'";
-
-  echo "\n\rquery: ".$sqlquery."\n\r";
+  
   /*if ($result = $mysqli->query($sqlquery)) {
       // printf("Select returned %d rows.\n", $result->num_rows);
       while($row = $result->fetch_assoc()) {
