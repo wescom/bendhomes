2605012e25c78f27b1c72013cb1997f5b4f5bbd1
jarelsc <jculley@wescompapers.com>
2016-10-03 10:35:27 -0700

    edit single agent page

--- wp-content/themes/realhomes/single-agent.php
+++ wp-content/themes/realhomes/single-agent.php
@@ -72,7 +72,13 @@ get_header();
                                                     if(!empty($agent_office_phone)){
                                                         ?><li class="office">
                                                         <?php include( get_template_directory() . '/images/icon-phone.svg' ); _e('Office', 'framework'); ?> : 
-                                                        <?php echo '<a href="tel:'. str_replace("-", '', $agent_office_phone) .'">'. $agent_office_phone .'</a>'; ?>
+                                                        <?php 
+														if($featured_agent == 1) {
+															echo '<a href="tel:'. str_replace("-", '', $agent_office_phone) .'">'. $agent_office_phone .'</a>';
+                                                        } else {
+                                                        	echo $agent_office_phone;
+                                                        }
+														?>
                                                         </li><?php
                                                     }
                                                     if(!empty($agent_mobile) && $featured_agent == 1){
