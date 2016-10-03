2563e81a95820f95a4bc821b2b0a66a045a87e9d
jcontreras28 <janellec@bendbroadband.com>
2016-10-03 14:17:06 -0700

    delete properties code

--- wp-content/plugins/bh-importer/functions.php
+++ wp-content/plugins/bh-importer/functions.php
@@ -657,12 +657,12 @@ function bhDeleteProperty($propItem){
     
     $imagePath = ABSPATH.'_retsapi/images/property/'.$fileStem[0];
     echo " Delete image: ".$imagePath."</p>";
-    /*foreach( glob($imagePath.'*') as $file ) {
+    foreach( glob($imagePath.'*') as $file ) {
       if(file_exists($file)) {
-        echo $file;
+        echo " found: ".$file;
         //unlink($file);
       }
-    }*/
+    }
     $imagePath = ABSPATH.'_retsapi/imagesbackup/property/'.$fileStem[0];
     echo " Delete image: ".$imagePath."</p>";
     /*foreach( glob($imagePath.'*') as $file ) {
