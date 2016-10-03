c3c9f01f7af4d3137906d89214acef92e8b029fa
jarelsc <jculley@wescompapers.com>
2016-10-03 10:45:57 -0700

    edit agent search results, add mobile phone if featured

--- wp-content/themes/realhomes/bend-homes/template-parts/search-agent.php
+++ wp-content/themes/realhomes/bend-homes/template-parts/search-agent.php
@@ -5,16 +5,21 @@ global $post;
 $id = get_the_ID();
 $permalink = get_permalink();
 $title = get_the_title();
-		
+$featured_agent = get_field( 'agent_is_featured' );
 $image_size = 'agent-image';
 $brokerage = get_field( 'brk_office_name' );
 $category_classes = sanitize_title( strip_tags( get_the_term_list( $id, 'agent_types', '', ' ', '' ) ) );
 $address = get_field( 'brk_office_address' );
 $phone = get_field( 'brk_office_phone' );
+$mobile_phone = get_field( 'REAL_HOMES_mobile_number' );
 if( $phone )
 	$phone = sprintf( '<div class="phone"><i class="fa fa-mobile"></i> <a href="tel:%s">%s</a></div>', preg_replace("/[^0-9]/", "", $phone), $phone );
+if( $mobile_phone )
+	$mobile_phone = sprintf( '<div class="mobile"><i class="fa fa-mobile"></i> <a href="tel:%s">%s</a></div>', preg_replace("/[^0-9]/", "", $mobile_phone), $mobile_phone );
+	
+$mobile = $featured_agent == 1 ? $mobile_phone : '';
 				
-$additional_meta = sprintf( '<div class="extra-meta agent-meta"><div>%s<div>%s</div></div>%s</div>', $brokerage, $address, $phone );
+$additional_meta = sprintf( '<div class="extra-meta agent-meta"><div>%s<div>%s</div></div>%s%s</div>', $brokerage, $address, $phone, $mobile );
 
 $image = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), $image_size, true);
 $image_parts = pathinfo( $image[0] );
