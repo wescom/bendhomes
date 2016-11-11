8117ffbd34af6456c55487c40b9d4a315f2ede21
jarelsc <jculley@wescompapers.com>
2016-11-11 14:24:06 -0800

    shortcode

--- wp-content/plugins/tbb-functions/tbb-shortcodes.php
+++ wp-content/plugins/tbb-functions/tbb-shortcodes.php
@@ -1279,7 +1279,7 @@ function tbb_mortgage_calc_form_js( $atts ) {
 	var downpayment = document.mortgagecalc.down.value;
 	var percentdown = (downpayment / price) * 100;
 	
-	document.getElementById('down-percent').innerHTML = percentdown.toFixed(2)+'%';
+	document.getElementById('down-percent').innerHTML = percentdown.toFixed(2)+'% Down';
 	}
 
 	function myPayment()
@@ -1331,10 +1331,10 @@ function tbb_mortgage_calc_form_js( $atts ) {
 			<div class="smpc-div">
 			<form name=mortgagecalc method=POST>
 			<p>Listing Price<br>
-			<input id="mort-price-value" type=text onkeypress="return validNumber(event)" onChange="findpercentdown()" name=price size=10 value="<?php echo $sale_price; ?>"> <span class="smpc-error" id="priceError"></span>
+			<input id="mort-price-value" type=text onkeypress="return validNumber(event)" onChange="findpercentdown()" onkeyup="this.onchange();" name=price size=10 value="<?php echo $sale_price; ?>"> <span class="smpc-error" id="priceError"></span>
 			</p>
 			<p>Down Payment<br>
-			<input id="mort-down-value" type=text onkeypress="return validNumber(event)" onChange="findpercentdown()" name=down size=10 value="<?php echo $down_payment; ?>"> <span id="down-percent"><?php echo $down_percent; ?>%</span> <span class="smpc-error" id="downError"></span></p>
+			<input id="mort-down-value" type=text onkeypress="return validNumber(event)" onChange="findpercentdown()" onkeyup="this.onchange();" name=down size=10 value="<?php echo $down_payment; ?>"> <span id="down-percent"><?php echo $down_percent; ?>%</span> <span class="smpc-error" id="downError"></span></p>
 			<p>Term (in years)?<br>
 			<input id="mort-term-value" type=text onkeypress="return validNumber(event)" name=years size=5 value="<?php echo $year_term; ?>"> <span class="smpc-error" id="yearsError"></span></p>
 			<p>Interest Rate<br>
