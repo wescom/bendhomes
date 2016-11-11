9b4fcbdb6df8456a2a4eb648e0120c45b3176820
jarelsc <jculley@wescompapers.com>
2016-11-11 13:59:52 -0800

    shortcode

--- wp-content/plugins/tbb-functions/tbb-shortcodes.php
+++ wp-content/plugins/tbb-functions/tbb-shortcodes.php
@@ -1272,6 +1272,15 @@ function tbb_mortgage_calc_form_js( $atts ) {
 	  }
 	  return x1 + x2;
 	}
+		
+	function findpercentdown()
+	{
+	var price = document.mortgagecalc.price.value;
+	var downpayment = document.mortgagecalc.down.value;
+	var percentdown = downpayment / price;
+	
+	document.getElementById('down-percent').innerHTML = percentdown.toFixed(0)+'%';
+	}
 
 	function myPayment()
 	{
@@ -1284,21 +1293,17 @@ function tbb_mortgage_calc_form_js( $atts ) {
 	// Form validation checking
 	if ((document.mortgagecalc.price.value === null) || (document.mortgagecalc.price.value.length === 0) || (isNaN(document.mortgagecalc.price.value) === true))
 	{
-	document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
 	document.getElementById('priceError').innerHTML = 'Numeric value required. Example: 165000';
 	}
 	else if ((document.mortgagecalc.down.value === null) || (document.mortgagecalc.down.value.length === 0) || (isNaN(document.mortgagecalc.down.value) === true))
 	{
-	document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
-	document.getElementById('downError').innerHTML = 'Numeric value required. Example: 165000';
+	document.getElementById('downError').innerHTML = 'Numeric value required. Example: 50000';
 	} else if ((document.mortgagecalc.years.value === null) || (document.mortgagecalc.years.value.length === 0) || (isNaN(document.mortgagecalc.years.value) === true))
 	{
-	document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
 	document.getElementById('yearsError').innerHTML = 'Numeric value required. Example: 30';
 	} else if ((document.mortgagecalc.rate.value === null) || (document.mortgagecalc.rate.value.length === 0) || (isNaN(document.mortgagecalc.rate.value) === true))
 	{
-	document.getElementById('monthlyPayment').innerHTML = 'Please enter the missing information.';
-	document.getElementById('rateError').innerHTML = 'Numeric value required. Example: 5.25';
+	document.getElementById('rateError').innerHTML = 'Numeric value required. Example: 3.25';
 	} else
 	{
 	// Set variables from form data
@@ -1329,7 +1334,7 @@ function tbb_mortgage_calc_form_js( $atts ) {
 			<input type=text onkeypress="return validNumber(event)" name=price size=10 value="<?php echo $sale_price; ?>"> <span class="smpc-error" id="priceError"></span>
 			</p>
 			<p>Down Payment<br>
-			<input type=text onkeypress="return validNumber(event)" name=down size=10 value="<?php echo $down_payment; ?>"> <span class="smpc-error" id="downError"></span></p>
+				<input type=text onkeypress="return validNumber(event)" onKeyUp="return findpercentdown()" name=down size=10 value="<?php echo $down_payment; ?>"> <span id="down-percent"> </span> <span class="smpc-error" id="downError"></span></p>
 			<p>Term (in years)?<br>
 			<input type=text onkeypress="return validNumber(event)" name=years size=5 value="<?php echo $year_term; ?>"> <span class="smpc-error" id="yearsError"></span></p>
 			<p>Interest Rate<br>
@@ -1338,7 +1343,6 @@ function tbb_mortgage_calc_form_js( $atts ) {
 			</form>
 			<small>Instructions: Enter numbers and decimal points. No commas or other characters.</small>
 			</div>
-			<div id="monthlyPayment"></div>
 			
 		</div><!-- end class mort-calc -->
 	</div><!-- end class mort-calc-form-wrap -->
