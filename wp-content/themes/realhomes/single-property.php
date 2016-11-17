ca4c91777fc211259206665cd227c9c7f988070a
jarelsc <jculley@wescompapers.com>
2016-11-17 12:46:22 -0800

    single prop

--- wp-content/themes/realhomes/single-property.php
+++ wp-content/themes/realhomes/single-property.php
@@ -140,7 +140,7 @@ get_template_part('bend-homes/property-details/property-agent-for-sidebar');
 						</div>
 						<script type="text/javascript">
 							$('a[href="#tab-map"]').on('shown', function(){
-								google.maps.event.trigger(propertyMap, 'resize');
+								initialize_property_map();
 							 });
 						</script>
 					</div>
