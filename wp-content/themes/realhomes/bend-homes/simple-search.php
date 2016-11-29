b61d202d70878a7032ae2d78327d904b89901a69
jarelsc <jculley@wescompapers.com>
2016-11-29 14:25:51 -0800

    css

--- wp-content/themes/realhomes/bend-homes/simple-search.php
+++ wp-content/themes/realhomes/bend-homes/simple-search.php
@@ -2,13 +2,13 @@
 <div class="simple-search-wrap">
     <form class="simple-search-form clearfix" action="<?php global $theme_search_url; echo $theme_search_url; ?>" method="get">
     
-		<div class="option-bar large">
+		<div class="option-bar input">
 			<label for="keyword-txt"><?php _e('Keyword', 'framework'); ?></label>
 			<input type="text" name="keyword" id="keyword-txt" value="<?php echo isset ( $_GET['keyword'] ) ? $_GET['keyword'] : ''; ?>" placeholder="<?php _e('Search Any Property', 'framework'); ?>" />
 		</div>
 		
 		
-	<div class="option-bar">
+	<div class="option-bar submit">
         <input type="submit" value="<?php _e('Search', 'framework'); ?>" class=" real-btn btn">
     </div>
     
