3d4d0f142be6c85f1acd6ad1c128bd96bf4e713b
jarelsc <jculley@wescompapers.com>
2016-11-29 15:12:40 -0800

    single prop

--- wp-content/themes/realhomes/single-property.php
+++ wp-content/themes/realhomes/single-property.php
@@ -37,15 +37,15 @@ get_template_part('bend-homes/property-details/property-agent-for-sidebar');
     ?>
 
     <!--div class="page-spacer"></div-->
-    <div class="simple-search-wrap">
-   <div class="container">
-	<div class="row">
-    <?php
-	get_template_part('bend-homes/advance-search');
-	?>
-	   </div>
+	<div class="simple-search-wrap">
+		<div class="container">
+			<div class="row">
+				<?php
+				get_template_part('bend-homes/advance-search');
+				?>
+			</div>
 		</div>
-</div>
+	</div>
     
     <!-- Content -->
     <div class="container contents detail">
