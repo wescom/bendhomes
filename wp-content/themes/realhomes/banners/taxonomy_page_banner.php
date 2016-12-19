c6dbf0e6b0698cd4bdbe52734aaed9d3250a7b53
jarelsc <jculley@wescompapers.com>
2016-12-19 10:11:11 -0800

    fix taxonomy headlines

--- wp-content/themes/realhomes/banners/taxonomy_page_banner.php
+++ wp-content/themes/realhomes/banners/taxonomy_page_banner.php
@@ -7,14 +7,29 @@
     $banner_title = $current_term->name;
 
     // Banner Sub Title
-    $banner_sub_title = $current_term->description;
+    $banner_sub_title = !empty($current_term->description) ? ' | '. $current_term->description : '';
+
+	switch( is_tax() ) {
+		case 'elementary_school' :
+			$banner_sub_title = ' Elementary School';
+			break;
+		case 'middle_school' :
+			$banner_sub_title = ' Middle School';
+			break;
+		case 'high_school' :
+			$banner_sub_title = ' High School';
+			break;
+		case 'county' :
+			$banner_sub_title = ' County';
+			break;
+	}
     ?>
     <?php /* <div class="page-head" style="background-repeat: no-repeat;background-position: center top;background-image: url('<?php echo $banner_image_path; ?>'); background-size: cover; "> */ ?>
     <div class="page-head">
         <?php if(!('true' == get_option('theme_banner_titles'))): ?>
         <div class="container">
             <div class="wrap clearfix">
-                <h1 class="page-title"><?php echo $banner_title .' | '. $banner_sub_title; ?></h1>
+                <h1 class="page-title"><?php echo $banner_title .''. $banner_sub_title; ?></h1>
             </div>
         </div>
         <?php endif; ?>
