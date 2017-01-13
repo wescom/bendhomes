be0a0bac61b367041bb0d13e015cfc41a2635ba4
jcontreras28 <janellec@bendbroadband.com>
2017-01-13 11:47:03 -0800

    daata

--- wp-content/themes/realhomes/template-hide-rets-bad-status.php
+++ wp-content/themes/realhomes/template-hide-rets-bad-status.php
@@ -49,12 +49,10 @@ function hide_item($item) {
 
         $query = 'update wp_posts set post_status = "private" where ID = '.$postId; // from wp_postmeta where meta_value = "'.$item.'"';
         echo " - query: ".$query;
-        $result = $conn->query($query);
-        echo '- did it.';
+        //$result = $conn->query($query);
+        //echo '- did it.';
 
 }
-
-echo '<p style="background-color: brown; color: #ffffff; padding: 0.25em;">hello world!</p>';
 
 $ourFile = ABSPATH.'/_retsapi/IdTextFiles/badIds.txt';
 $ourString = file_get_contents($ourFile);
@@ -79,42 +77,5 @@ foreach($ourArray as $item) {
 	echo '</p>';
 }
 
-
-
-//$daysBack = 365;
-
-//theTm = time();
-//bh_write_to_log('Entered template-delete-old-properties.php ','propertiesDeleteEntry'.$theTm);
-
-/*foreach($scenarios as $scenario) {
-
-	$resource = $scenario['resource'];
-  	$class = $scenario['class'];
-  	$rc = $resource.'_'.$class;  // ie:  Property_RESI
-
-	$propList = dbDeleteOldIdList($scenario, $rc, $daysBack);
-	$log = $rc." Found ".count($propList);
-	bh_write_to_log($log,'propertiesDeleteEntry'.$theTm);
-	echo "<h1 style='color:green'>".$log."</h3>";
-	foreach($propList as $propItem) {
-
-	 	$mlsposts = bhLookupPostByMLS($propItem['MLNumber']);
-	 	$bhpropertyid = $mlsposts[0];
-	 	$log = $propItem['MLNumber']." wpID: ".$bhpropertyid." status: ".$propItem['Status']." lastMod: ".$propItem["LastModifiedDateTime"];
-	    echo "<p>mls: ".$log."</p>";
-
-	    $wasSuccess = bhDeleteProperty($propItem, $rc);
-
-	    if ($bhpropertyid > 0) { 
-	    	bhDeleteWPImages($bhpropertyid);
-	    	wp_delete_post($bhpropertyid);
-	    	
-	    } else {
-	    	echo "<p>Property was not in wordpress database";
-	    }
-	}
-}*/
-
-//bh_write_to_log('Completed template-delete-old-properties.php ','propertiesDeleteEntry'.$theTm);
 
 ?>
