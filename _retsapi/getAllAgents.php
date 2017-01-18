86261a25fe6efbe2868badade7c6948bf83722ae
jcontreras28 <janellec@bendbroadband.com>
2017-01-18 15:10:18 -0800

    agents

--- _retsapi/getAllAgents.php
+++ _retsapi/getAllAgents.php
@@ -255,7 +255,7 @@ echo '<h1 style="border: 3px solid orange; padding: 3px;">start - '.date(DATE_RS
 
 $updateByIdListFile = true;  // only used manually to pull all data in from text file of ids
 
-if ($updateByIdListFile == false) {
+if ($updateByIdListFile == true) {
   $pullDate = '2001-01-01T00:00:00-08:00'; //set this to however far back you want to pull from
 } else {
   $pullDate = getSetPullDate();
