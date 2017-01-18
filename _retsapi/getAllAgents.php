65b57581db71f601de30fb7a1e2247c8f5361028
jcontreras28 <janellec@bendbroadband.com>
2017-01-18 14:13:27 -0800

    agents

--- _retsapi/getAllAgents.php
+++ _retsapi/getAllAgents.php
@@ -58,7 +58,7 @@ function getSetPullDate() {
     $pulldate['recent'] = strtotime("-1 day"); // 1 day, 2 days, 1 year, 2 years, 1 week, 2 weeks, etc
   }
 
-  $pulldate['retsquery'] = '2002-01-16T00:00:00-08:00'; //date('c',$pulldate['recent']);
+  $pulldate['retsquery'] = '2017-01-16T00:00:00-08:00'; //date('c',$pulldate['recent']);
   echo '<p style="background-color: orange;">using date: '.$pulldate['retsquery'].'</p>';
   return $pulldate['retsquery'];
 }
@@ -253,7 +253,7 @@ foreach($scenarios as $qvars) {
   // 1. Get ids of Agents that have updated since last pull date
    $idList = runRetsQuery($qvars, $pullDate);
    echo '<pre>';
-   print_r($idList);
+   //print_r($rets_data);
    echo '</pre>';
    $all_agent_data = getAllAgentData($qvars, $pullDate, $idList);
    echo '<pre>';
