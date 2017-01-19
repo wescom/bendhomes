cba197d89a440070b94f6a4928a029ec773c95d7
jarelsc <jculley@wescompapers.com>
2017-01-19 13:35:42 -0800

    order by random test

--- wp-content/plugins/tbb-functions/rets-shortcodes.php
+++ wp-content/plugins/tbb-functions/rets-shortcodes.php
@@ -100,7 +100,7 @@ class Rets_Agents {
 			LEFT JOIN Agent_MEMB on ActiveAgent_MEMB.MemberNumber = Agent_MEMB.MemberNumber
 			LEFT JOIN Office_OFFI on ActiveAgent_MEMB.OfficeNumber = Office_OFFI.OfficeNumber
 			WHERE ActiveAgent_MEMB.OfficeNumber <> 99999 
-			{$sort_order}
+			ORDERBY FullName RAND()
 		");
 		
 		//print_r( $agents );
