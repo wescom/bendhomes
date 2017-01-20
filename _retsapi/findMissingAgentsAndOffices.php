e6b2b482058cbe467c7e268c7574068f23b7936c
jcontreras28 <janellec@bendbroadband.com>
2017-01-20 09:51:25 -0800

    dev on missing agents and offices file

--- _retsapi/findMissingAgentsAndOffices.php
+++ _retsapi/findMissingAgentsAndOffices.php
@@ -1,7 +1,7 @@
 <?php
 //include("inc/retsabspath.php");
 include("/var/www/html/_retsapi/inc/header.php");
-include("var/www/html/_retsapi/AgentOfficesFunctions");
+include("/var/www/html/_retsapi/AgentOfficesFunctions.php");
 ini_set('max_execution_time', 0);
 
 $centralcount = 999999;
