386950a41d0aee7e153018cb19c1c7e87043ca4c
jcontreras28 <janellec@bendbroadband.com>
2017-01-20 10:55:41 -0800

    dev on missing agents and offices file

--- _retsapi/AgentOfficesFunctions.php
+++ _retsapi/AgentOfficesFunctions.php
@@ -285,6 +285,54 @@ function getSetPullDate() {
   return $pulldate['retsquery'];
 }
 
+function deletBadIds($badIdsArray, $qvars) {
+  $db = array(
+    'host' => 'localhost',
+    'username' => 'phrets',
+    'password' => 'hCqaQvMKW9wJKQwS',
+    'database' => 'bh_rets'
+  );
+
+  if ($qvars['class'] == "OFFI") {
+    $idType = "OfficeNumber";
+  } else {
+    $idType = "MemberNumber";
+  }
+
+  $conn = new mysqli($db['host'], $db['username'], $db['password'], $db['database']);
+
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+  $query = "DELETE from ".$qvars['resource'].'_'.$qvars['class']." WHERE ".$idType." IN (".implode(", ",$badIdsArray).")";
+  echo '<p>'.$query.'</p>';
+  
+  if($conn->query($query)) {
+    echo "<p>Success!!!!</p>";
+  } else {
+    echo "<p>Error: ".mysqli_error($conn)."</p>";
+  }
+  mysqli_close($conn);
+}
+
+
+function compareAndGetBads($retsIdArray, $ourIdArray) {
+  // anything that is in ours but not rets, should be deleted from ours
+  $badIds = "";
+  $idArray = [];
+  foreach($ourIdArray as $item) {
+    if (in_array($item, $retsIdArray)){
+        //echo "<pre>".$item." - good</pre>";
+      } else {
+        array_push($idArray, $item);
+      }
+  }
+  echo '<pre style="color: red;">BAD Ids - count: '.sizeof($idArray).' - '.implode(",",$idArray).'</pre>';
+  return $idArray;
+}
+
+
 function executeGetAgentsAndOffices() {
 
     echo '<h1 style="border: 3px solid orange; padding: 3px;">start - '.date(DATE_RSS).' - v2100</h1>';
@@ -354,6 +402,31 @@ function executeGetAgentsAndOffices() {
 
     echo '<h1 style="border: 3px solid orange; color: green; padding: 3px;">completed - '.date(DATE_RSS).'</h1>';
 
+}
+
+function executeCleanAgentsAndOfficesTables() {
+
+  $scenarios = getScenarios();
+
+  $pullDate = '2001-01-01T00:00:00-08:00';
+
+  foreach($scenarios as $qvars) {
+
+    $retsIdArray = runRetsQuery($qvars, $pullDate);
+
+
+    $ourIdArray = getOurIds($qvars);
+
+
+    $badIdsArray = compareAndGetBads($retsIdArray, $ourIdArray);
+
+    if (sizeof($badIdsArray) > 0) {
+      deletBadIds($badIdsArray, $qvars);
+    } else {
+      echo '<p>Empty array - no bad ids to delete...</p>';
+    }
+
+  }
 }
 
 ?>
--- _retsapi/cleanAgentsAndOfficesTables.php
+++ _retsapi/cleanAgentsAndOfficesTables.php
@@ -1,78 +1,8 @@
 <?php
-//include("inc/retsabspath.php");
-include("/var/www/html/_retsapi/inc/header.php");
+
 include("/var/www/html/_retsapi/AgentOfficesFunctions.php");
-ini_set('max_execution_time', 0);
 
-/* ##### ######### ##### */
-/* ##### FUNCTIONS ##### */
-/* ##### ######### ##### */
+executeCleanAgentsAndOfficesTables();
 
-function deletBadIds($badIdsArray, $qvars) {
-  $db = array(
-    'host' => 'localhost',
-    'username' => 'phrets',
-    'password' => 'hCqaQvMKW9wJKQwS',
-    'database' => 'bh_rets'
-  );
-
-  if ($qvars['class'] == "OFFI") {
-    $idType = "OfficeNumber";
-  } else {
-    $idType = "MemberNumber";
-  }
-
-  $conn = new mysqli($db['host'], $db['username'], $db['password'], $db['database']);
-
-  if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-  }
-
-  $query = "DELETE from ".$qvars['resource'].'_'.$qvars['class']." WHERE ".$idType." IN (".implode(", ",$badIdsArray).")";
-  echo '<p>'.$query.'</p>';
-  
-  if($conn->query($query)) {
-    echo "<p>Success!!!!</p>";
-  } else {
-    echo "<p>Error: ".mysqli_error($conn)."</p>";
-  }
-  mysqli_close($conn);
-}
-
-
-function compareAndGetBads($retsIdArray, $ourIdArray) {
-  // anything that is in ours but not rets, should be deleted from ours
-  $badIds = "";
-  $idArray = [];
-  foreach($ourIdArray as $item) {
-    if (in_array($item, $retsIdArray)){
-        //echo "<pre>".$item." - good</pre>";
-      } else {
-        array_push($idArray, $item);
-      }
-  }
-  echo '<pre style="color: red;">BAD Ids - count: '.sizeof($idArray).' - '.implode(",",$idArray).'</pre>';
-  return $idArray;
-}
-
-$pullDate = '2001-01-01T00:00:00-08:00';
-
-foreach($scenarios as $qvars) {
-
-  $retsIdArray = runRetsQuery($qvars, $pullDate);
-
-
-  $ourIdArray = getOurIds($qvars);
-
-
-  $badIdsArray = compareAndGetBads($retsIdArray, $ourIdArray);
-
-  if (sizeof($badIdsArray) > 0) {
-    deletBadIds($badIdsArray, $qvars);
-  } else {
-    echo '<p>Empty array - no bad ids to delete...</p>';
-  }
-
-}
 
 ?>
