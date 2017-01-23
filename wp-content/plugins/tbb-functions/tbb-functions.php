37f460f6d97b65314d642575783fd15d05a77ef1
jarelsc <jculley@wescompapers.com>
2017-01-23 12:53:21 -0800

    comment out agent post type registration

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -41,9 +41,9 @@ require_once( TBB_FUNCTIONS_DIR . 'rets-connect.class.php' );
 require_once( TBB_FUNCTIONS_DIR . 'admin/offices.php' );
 require_once( TBB_FUNCTIONS_DIR . 'admin/agents.php' );
 
-require_once( TBB_FUNCTIONS_DIR . 'post-types/post-type-company.php' );
+//require_once( TBB_FUNCTIONS_DIR . 'post-types/post-type-company.php' );
 //require_once( TBB_FUNCTIONS_DIR . 'admin/settings-company.php' );
-require_once( TBB_FUNCTIONS_DIR . 'admin/settings-agents.php' );
+//require_once( TBB_FUNCTIONS_DIR . 'admin/settings-agents.php' );
 
 require_once( TBB_FUNCTIONS_DIR . 'tbb-shortcodes.php' );
 require_once( TBB_FUNCTIONS_DIR . 'rets-shortcodes.php' );
