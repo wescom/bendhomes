5f2f6354575f29b9e24badf8d778355fe97ff086
jcontreras28 <janellec@bendbroadband.com>
2017-01-23 13:20:55 -0800

    grab agents by chunk

--- _retsapi/AgentOfficesFunctions.php
+++ _retsapi/AgentOfficesFunctions.php
@@ -401,7 +401,7 @@ function executeGetAgentsAndOffices() {
         file_put_contents($file, implode(",", $idArray));*/
         // ***********  End part 1 ***********
         // *********** 2. this is second step, use the ids you got previous and chunk them up in reasonable imports ************
-        $start = 1200; // start index
+        $start = 1400; // start index
         $count = 200; // how many past start to grab
         $idFile = "./IdTextFiles/".$qvars['resource'].'.txt';;
         $idString = file_get_contents($idFile);
