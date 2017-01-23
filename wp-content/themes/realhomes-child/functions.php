9b09ac935a34d13407fe2b35e4674405008e788b
jarelsc <jculley@wescompapers.com>
2017-01-23 11:44:32 -0800

    redirect function

--- wp-content/themes/realhomes-child/functions.php
+++ wp-content/themes/realhomes-child/functions.php
@@ -426,8 +426,7 @@ if(is_admin()) new PropertySettingsPage;
 // Uncomment this action out to get logged in, then reactivate it again.
 add_action( 'init', 'bh_redirect_non_admin_user' );
 function bh_redirect_non_admin_user(){
-	global $pagenow;
-    if ( !defined( 'DOING_AJAX' ) && !current_user_can('administrator') && !$pagenow === 'wp-login.php' ){
+    if ( !defined( 'DOING_AJAX' ) && !current_user_can('administrator') ){
         wp_redirect( 'http://adhosting.wescompapers.com/bendhomes-com/' );  
 		exit;
     } 
