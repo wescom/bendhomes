556c048f3d46cfa7b5847d488b0efec8eec84c67
jcontreras28 <janellec@bendbroadband.com>
2017-01-23 16:47:38 -0800

    companies shortcode

--- wp-content/plugins/tbb-functions/rets-shortcodes.php
+++ wp-content/plugins/tbb-functions/rets-shortcodes.php
@@ -820,7 +820,7 @@ class Rets_Company_Agents {
 							document.getElementById("sort-order").onchange = function() { if (this.selectedIndex!==0) { window.location.href = this.value; } };
 							</script>';
 			
-				$html .= '<div class="agents-list-wrap clearfix">';
+				//$html .= '<div class="agents-list-wrap clearfix">';
 				foreach( $agents as $agent ) {
 										
 					$category_classes = $agent['featured'] == 1 ? 'featured' : 'not-featured';
@@ -837,16 +837,16 @@ class Rets_Company_Agents {
 					
 					$permalink = home_url() .'/'. $linkto .'/?agent='. $this->create_slug( $agent['FullName'] ) .'&id='. $agent['MemberNumber'];
 														
-						$html .= '<div class="company-agent">';
+						/*$html .= '<div class="company-agent">';
 						$html .= '<a class="company-agent-inner" href="'.$permalink.'">';
 						$html .= '<figure class="agent-image">'
 						$html .= sprintf('<img src="%s" alt="%s" width="" height="" />', $image_url, $agent_name);
 						$html .= '</figure>';                                                        
 						$html .= '<div class="agent-name">'.$agent['FullName'].'</div>';
-						$html .= '</a></div></div>';
+						$html .= '</a></div></div>';*/
 
 					// Begin agent output
-					/*$html .= sprintf( '<div class="custom-post custom-post-%s %s %s %s %s"><div class="custom-post-item clearfix">', 
+					$html .= sprintf( '<div class="custom-post custom-post-%s %s %s %s %s"><div class="custom-post-item clearfix">', 
 							$count, $cols, $class, $has_image_class, $category_classes );
 					
 						$html .= sprintf( '<figure class="custom-post-image image-agent-image-%s"><a href="%s"><img src="%s" width="" height="" alt="%s, for %s" /></a></figure>', 
@@ -860,7 +860,7 @@ class Rets_Company_Agents {
 						$html .= sprintf( '<a class="more-details" href="%s">More Details <i class="fa fa-caret-right"></i></a>', $permalink );
 					
 					$html .= '</div></div>';
-					// End agent ouput*/
+					// End agent ouput
 					
 					$clearfix_test = $count / $cols_per_row;
 					if( is_int( $clearfix_test ) ) {
