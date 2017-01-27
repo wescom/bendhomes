eabe19bf4295cc4cbc29bd09402bb6ec579684c3
jarelsc <jculley@wescompapers.com>
2017-01-27 12:22:24 -0800

    edit featured agent html/css

--- _retsapi/propertyAgentWidget.php
+++ _retsapi/propertyAgentWidget.php
@@ -83,11 +83,11 @@ include_once '/var/databaseIncludes/retsDBInfo.php';
                 }
 
                 $returnText = '<section class="agent-widget clearfix">';
+				$returnText .=  '<h3 class="title">Listing Agent:<div><strong><a href="http://www.bendhomes.com/agent/?'.$agPageUrl.'&id='.$agId.'">'.$agName.'</a></strong></div></h3>';
                 $returnText .= '<a class="agent-image" href="http://www.bendhomes.com/agent/?agent='.$agPageUrl.'&id='.$agId.'">';
-                $returnText .=  '<image src="'.$agImage.'" />';
+                $returnText .=  '<image src="'.$agImage.'" alt="'.$agName.' for '.$agOfficeName.'" />';
                 $returnText .=  '</a><br>';
                 $returnText .=  '<div class="agent-info clearfix">';
-                $returnText .=  '<h3 class="title">Listing Agent: <strong><a href="http://www.bendhomes.com/agent/?'.$agPageUrl.'&id='.$agId.'">'.$agName.'</a></strong></h3>';
                 $returnText .=  '<div class="agent-office-name">'.$agOfficeName.'</div>';
                 $returnText .=  '<div class="contacts-list">';
                 if ($agOfficePhone != "")
