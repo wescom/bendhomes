c8c993cbc09dd51b2b890f9f2ee98dc30a40c946
jcontreras28 <janellec@bendbroadband.com>
2017-02-07 13:30:05 -0800

    ad dev work

--- wp-content/themes/realhomes-child/functions.php
+++ wp-content/themes/realhomes-child/functions.php
@@ -58,7 +58,7 @@ function child_theme_head_script() {
    		$urlLink = $_SERVER['REQUEST_URI'];
    		$link_array = explode('/',$urlLink);
     	$sectionKey = $link_array[count($link_array) - 2];  
-   		echo "sectionKey4 = ".$sectionKey;
+   		echo "sectionKey5 = ".$sectionKey;
    	}
    ?>
   <script type='text/javascript'>
