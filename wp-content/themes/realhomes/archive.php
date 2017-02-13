5c8bc5ebe393392998faac9cc37aa72458dbbbc5
jarelsc <jculley@wescompapers.com>
2017-02-13 11:51:18 -0800

    edit page-head

--- wp-content/themes/realhomes/archive.php
+++ wp-content/themes/realhomes/archive.php
@@ -50,7 +50,7 @@ get_header();
         <div class="container">
             <div class="wrap clearfix">
               <h1 class="page-title"><?php echo /* $banner_title .' | '.  */ $banner_details; ?>
-                 <?php if (function_exists('z_taxonomy_image_url')) {
+                 <?php if ( function_exists('z_taxonomy_image_url') && z_taxonomy_image_url() != '(unknown)' ) {
                   	echo '<img src="'. z_taxonomy_image_url() .'" style="width: 250px; height: auto;" />';
 				 } ?>
               </h1>
