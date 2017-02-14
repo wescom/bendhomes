a113117b415f351a768a90a2e2667e5fb9df2803
jcontreras28 <janellec@bendbroadband.com>
2017-02-14 12:10:38 -0800

    property data dev

--- _retsapi/propertiesFunctions.php
+++ _retsapi/propertiesFunctions.php
@@ -619,7 +619,7 @@ function executeUpdatePropertiesTable() {
     $pullDate = '2001-01-01T00:00:00-08:00';
 
     $start = 98000; // start index
-    $count = 2000; // how many past start to grab
+    $count = 100; // how many past start to grab
 
     foreach($scenarios as $qvars) {
 
