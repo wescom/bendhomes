0969ae7fa77bc46d8ccd6d2d2bdd1669b6321f7c
jcontreras28 <janellec@bendbroadband.com>
2017-02-17 15:58:47 -0800

    data import dev

--- _retsapi/propertiesFunctions.php
+++ _retsapi/propertiesFunctions.php
@@ -877,7 +877,7 @@ function executeUpdatePropertiesTable() {
     //$pullDate = getSetPullDate("-3 hours");
 
     //$start = 0;
-    $start = 64500; // start index
+    $start = 64000; // start index
     $count = 500; // how many past start to grab
 
     foreach($scenarios as $qvars) {
