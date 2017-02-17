037bbb378454155013017ef9bd598dcea77a6f2f
jcontreras28 <janellec@bendbroadband.com>
2017-02-16 16:17:55 -0800

    properties table dev

--- _retsapi/updatePropertiesTable.php
+++ _retsapi/updatePropertiesTable.php
@@ -2,12 +2,12 @@
 
         include_once("/var/www/html/_retsapi/propertiesFunctions.php");
 
-        //executeUpdatePropertiesTable();
+        executeUpdatePropertiesTable();
 
         //cleanPropertiesTable();
 
-        executeUpdateOpenHousesTable();
+        //executeUpdateOpenHousesTable();
 
-        cleanOpenHousesTable();
+        //cleanOpenHousesTable();
 
 ?>
