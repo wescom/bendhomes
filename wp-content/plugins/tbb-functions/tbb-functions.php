609a4eecdea9db0a12482378f3bf1ad17b22c788
jarelsc <jculley@wescompapers.com>
2017-02-20 12:29:08 -0800

    open houses js

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -230,7 +230,7 @@ function tbb_add_openhouses() {
 	var time = [];
 	var url_vars = getUrlVars();
 		console.log(url_vars);
-		console.log(url_vars[0]);
+		console.log(url_vars['dt0']);
 	for(var i in url_vars) {
 		//alert(i + " == " + url_vars[i]);
 		textNode = decodeURI(url_vars[i]);
