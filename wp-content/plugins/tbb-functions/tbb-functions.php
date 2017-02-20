346a6837cc64a20f714242db9964ea8a5ec9ca1e
jarelsc <jculley@wescompapers.com>
2017-02-20 13:33:58 -0800

    open house js

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -237,7 +237,7 @@ function tbb_add_openhouses() {
 				time[time.length] = '<div class="time time-'+ i +'">'+ textNode.replace('+', ' ') +'</div>';
 			}
 		}		
-		console.log(time.toString(' '));
+		console.log(time.toString(""));
 		
 		description.insertAdjacentHTML('beforebegin', '<div id="OpenHouse" class="clearfix"><h3>Open House Times</h3>'+ time.toString(' ') +'</div>');
 	}
