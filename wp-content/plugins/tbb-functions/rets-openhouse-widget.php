b1a0f2b3947dd130485dca6aa0dcb38f85c4c90e
jarelsc <jculley@wescompapers.com>
2017-02-21 09:02:12 -0800

    open house dev

--- wp-content/plugins/tbb-functions/rets-openhouse-widget.php
+++ wp-content/plugins/tbb-functions/rets-openhouse-widget.php
@@ -3,7 +3,7 @@
 // File loaded from tbb-functions/tbb-functions.php in add_action('wp_footer', 'rets_footer_code') 
 // Displays Open House date & times on single property IDX page
 
-include_once '/wp-content/plugins/tbb-functions/rets-connect.clsss.php';
+/*include_once '/wp-content/plugins/tbb-functions/rets-connect.clsss.php';
 
 $mls = !empty( $_GET["mls"] ) ? $_GET["mls"] : '';
 
@@ -17,10 +17,10 @@ $openhouses_query = new Rets_DB();
 		
 $openhouses = $openhouses_query->select( $query );
 
-print_r($openhouses);
+print_r($openhouses);*/
 
 
-/*include_once '/var/databaseIncludes/retsDBInfo.php';
+include_once '/var/databaseIncludes/retsDBInfo.php';
 
 $mls = !empty( $_GET["mls"] ) ? $_GET["mls"] : '';
 //$mls = 201610228;
@@ -39,21 +39,26 @@ $query = "
 
 $result = $conn->query($query);
 
-print_r($result);
+while ($row = $result -> fetch_assoc()) {
+	$rows[] = $row;
+}
+return $rows;
+
+print_r($rows);
 
 //echo $query;
-$returnText = "";
+/*$returnText = "";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
 		$agFax = "";
 		while($row = $result->fetch_assoc()) {
 			
 		}
-}
+}*/
 
 mysqli_close($conn);
 
 $returnText = str_replace('"', '\"', $returnText);
 $returnText = str_replace('/', '\/', $returnText);
 //echo 'openHouseCallBack({"html":"'.$returnText.'"})';
-//echo '{"html": "'.$returnText.'"}';*/
+//echo '{"html": "'.$returnText.'"}';
