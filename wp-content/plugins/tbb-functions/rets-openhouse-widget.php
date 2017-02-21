4c2d92d2f1952f83b4d39561c0e69169f7f60091
jarelsc <jculley@wescompapers.com>
2017-02-21 13:03:52 -0800

    test

--- wp-content/plugins/tbb-functions/rets-openhouse-widget.php
+++ wp-content/plugins/tbb-functions/rets-openhouse-widget.php
@@ -3,17 +3,7 @@
 // File loaded from tbb-functions/tbb-functions.php in add_action('wp_footer', 'rets_footer_code') 
 // Displays Open House date & times on single property IDX page
 
-function create_openhouse_widget() {
-$openhouses_query = new Rets_DB();
-$openhouses = $openhouses_query->select( $query );
-
-print_r($openhouses);
-}
-
-create_openhouse_widget();
-
-
-/*include_once '/var/databaseIncludes/retsDBInfo.php';
+include_once '/var/databaseIncludes/retsDBInfo.php';
 
 $mls = !empty( $_GET["mls"] ) ? $_GET["mls"] : '';
 //$mls = 201610228;
@@ -39,12 +29,14 @@ if ($result->num_rows > 0) {
 	
 	$html .= '<div id="OpenHouse" class="clearfix"><h3>Open House Times</h3>';
 	
-		for($i = 0; $i < count($result); ++$i) {
+		print_r($result);
+	
+		/*for($i = 0; $i < count($result); ++$i) {
 			$date = $result[$i]['DateAndTime'];
 			$time = $result[$i]['TimeComments'];
 			
 			$html .= sprintf( '<div class="time">%s %s</div>', $date, $time );
-		}
+		}*/
 	
 		/*while( $rows = $result->fetch_assoc() ) {
 			print_r($rows);
@@ -58,7 +50,7 @@ if ($result->num_rows > 0) {
 			}
 		}*/
 	
-	/*$html .= '</div>';
+	$html .= '</div>';
 	
 } else {
 	$html .= '<div></div>';
@@ -70,5 +62,5 @@ $html = str_replace('"', '\"', $html);
 $html = str_replace('/', '\/', $html);
 
 echo 'openHouseRender({"html":"'. $html .'"})';
-//echo '{"html": "'.$html.'"}';*/
+//echo '{"html": "'.$html.'"}';
 
