aca5446a3529ca1a0c2468f0f29d1c0086577ba4
jarelsc <jculley@wescompapers.com>
2017-02-27 15:11:15 -0800

    test

--- wp-content/plugins/tbb-functions/admin/rets-offices.php
+++ wp-content/plugins/tbb-functions/admin/rets-offices.php
@@ -1,62 +1,7 @@
 <?php
 // Offices admin page
 
-if ( !defined( 'ABSPATH' ) ) exit;
- 
-class RETS_Offices {
- 
-    private static $instance;
- 
-    /**
-     * Main Instance
-     *
-     * @staticvar   array   $instance
-     * @return      The one true instance
-     */
-    public static function instance() {
-         if ( ! isset( self::$instance ) ) {
-             self::$instance = new self;
-			 self::$instance->add_offices_menu();
-		}
- 
-        return self::$instance;
-    }
- 
-    public function add_offices_menu() {
-          
-         add_menu_page(
-            'RETS Offices',
-            'Offices',
-            'manage_options',
-            'rets-offices',
-            array(
-                $this,
-                'rets_admin_page'
-            ),
-            'dashicons-building',
-            '20'
-        );
-    }
- 
-    public function rets_admin_page() {
-         // Echo the html here...
-		echo '<h1>RETS Offices</h1>';
-    }
- 
-}
-// Call the class and add the menus automatically. 
-$RETS_Offices = RETS_Offices::instance();
-
-
-
-
-
-
-
-
-
-
-/*class RETS_Featured_Companies {
+class RETS_Featured_Companies {
 	
 	function __construct() {
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
@@ -112,25 +57,6 @@ $RETS_Offices = RETS_Offices::instance();
                     <div id="sections">
                         <section id="company" class="tbb-tab active">
                             <form id="create-companies" method="post" action="<?php echo admin_url( 'admin.php' ); ?>" enctype="multipart/form-data">
-                                      
-                            <?php 
-							$query = "
-								SELECT Office_OFFI.IsActive,
-								Office_OFFI.MLSID,
-								Office_OFFI.OfficeName,
-								Office_OFFI.OfficeDescription,
-								Office_OFFI.DisplayName,
-								Office_OFFI.featured,
-								FROM Office_OFFI
-								WHERE IsActive = 'T'
-								ORDER BY OfficeName ASC
-							";
-							$companies_query = new Rets_DB();
-		
-							$companies = $companies_query->select( $query );
-
-							print_r( $companies); 
-							?>
                                                                                             
                             <p>
                                 <input type="hidden" name="action" value="companies_created" />
