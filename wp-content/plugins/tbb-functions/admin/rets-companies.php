6c9ad1792dbf60bc5fadc03cf993dd7ee67ec0e3
jarelsc <jculley@wescompapers.com>
2017-02-27 12:45:36 -0800

    admin offices page dev

--- wp-content/plugins/tbb-functions/admin/rets-companies.php
+++ wp-content/plugins/tbb-functions/admin/rets-companies.php
@@ -9,7 +9,7 @@ class RETS_Featured_Companies {
 		add_action( 'admin_action_offices', array( $this, 'offices_admin_action' ) );
 	}
 
-	function admin_menu() {
+	private function admin_menu() {
 		add_menu_page(
 			'Featured Offices',
 			'Offices',
@@ -24,11 +24,11 @@ class RETS_Featured_Companies {
 		);
 	}
 	
-	function enqueue_company_files() {
+	private function enqueue_company_files() {
 		wp_enqueue_style( 'company', TBB_FUNCTIONS_URL . 'css/company-settings.css' );
 	}
 	
-	function offices_admin_action() {
+	private function offices_admin_action() {
 		// Do posting function here that creates/updates the companies.
 		//$this->create_company_posts();
 	
@@ -37,7 +37,7 @@ class RETS_Featured_Companies {
 		exit();
 	}
 
-	function office_settings_do_page() { ?>
+	private function office_settings_do_page() { ?>
 		
         <div class="wrap tbb-company-page">
         	<h1>Featured Offices</h1>
@@ -57,6 +57,9 @@ class RETS_Featured_Companies {
                     <div id="sections">
                         <section id="company" class="tbb-tab active">
                             <form id="create-companies" method="post" action="<?php echo admin_url( 'admin.php' ); ?>" enctype="multipart/form-data">
+                            
+                            print_r( $this->get_offices_query() );
+                            
                             <p>
                                 <input type="hidden" name="action" value="companies_created" />
                                 <input id="company-submit" class="button-primary" type="submit" value="<?php _e( 'Update', 'tbb_company' ); ?>" />
@@ -67,9 +70,28 @@ class RETS_Featured_Companies {
             
             </div>
         </div>
+        
 	<?php }
 	
-	function create_company_posts() {}
+	private function get_offices_query() {
+		$query = "
+			SELECT Office_OFFI.IsActive,
+			Office_OFFI.MLSID,
+			Office_OFFI.OfficeName,
+			Office_OFFI.OfficeDescription,
+			Office_OFFI.DisplayName,
+			Office_OFFI.featured,
+			FROM Office_OFFI
+			WHERE IsActive = 'T'
+			ORDER BY OfficeName ASC
+		";
+		
+		$offices_query = new Rets_DB();
+		
+		$offices = $offices_query->select( $query );
+		
+		return $offices;
+	}
 	
 }
 
