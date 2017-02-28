0fbb1b66ddad5752e2a7fbcd0a1f93ec690ffb9d
jarelsc <jculley@wescompapers.com>
2017-02-28 11:32:59 -0800

    office admin

--- wp-content/plugins/tbb-functions/admin/rets-offices-page.php
+++ wp-content/plugins/tbb-functions/admin/rets-offices-page.php
@@ -9,12 +9,11 @@ if ($conn->connect_error) {
 die("Connection failed: " . $conn->connect_error);
 }*/
 
+include TBB_FUNCTIONS_DIR . 'rets-connect.class.php';
+
 $query = "
-	SELECT Office_OFFI.OfficeName,
-	Office_OFFI.OfficeDescription,
-	Office_OFFI.DisplayName,
-	Office_OFFI.featured,
-	FROM Office_OFFI
+	SELECT OF.OfficeName, OF.OfficeDescription, OF.DisplayName, OF.featured,
+	FROM Office_OFFI OF
 	WHERE IsActive = 'T'
 	ORDER BY OfficeName ASC
 ";
