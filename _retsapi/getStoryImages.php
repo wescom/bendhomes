d3f36993bd0701d63dffbce0b82aa98de93ca7d6
jcontreras28 <janellec@bendbroadband.com>
2017-08-09 10:38:19 -0700

    added home and garden

--- _retsapi/getStoryImages.php
+++ _retsapi/getStoryImages.php
@@ -1,43 +1,49 @@
 <?php
 
-$feedURL = "http://www.bendbulletin.com/csp/mediapool/sites/BendBulletin/Exports/mrss.csp?publication=BendBulletin&section=realestate&areas=topStory,listStories1,listStories2&imgwidth=940";
+$feedReURL = "http://www.bendbulletin.com/csp/mediapool/sites/BendBulletin/Exports/mrss.csp?publication=BendBulletin&section=realestate&areas=topStory,listStories1,listStories2&imgwidth=940";
+$feedHGURL = "http://www.bendbulletin.com/csp/mediapool/sites/BendBulletin/Exports/mrss.csp?publication=BendBulletin&section=homeandgarden&areas=topStory,listStories1,listStories2&imgwidth=940";
 
-$content = file_get_contents($feedURL);
-$x = new SimpleXmlElement($content);
+$urlArray = [$feedReURL, $feedHGURL];
 
-echo "<ul>";
-foreach($x->channel->item as $entry)
+foreach ($urlArray as $url)
 {
-        echo "<li>".$entry->title;
+        $content = file_get_contents($url);
+        $x = new SimpleXmlElement($content);
 
-        $ns_media = $entry->children('http://search.yahoo.com/mrss/');
-
-
-        if (isset($ns_media->content));
+        echo "<ul>";
+        foreach($x->channel->item as $entry)
         {
-                $image = (string)$ns_media->content->attributes()['url'];
+                echo "<li>".$entry->title;
 
-                echo " --- ".$image." ---";
-                if ($image != "")
+                $ns_media = $entry->children('http://search.yahoo.com/mrss/');
+
+
+                if (isset($ns_media->content));
                 {
-                        $guid = $entry->guid;
-                        $guidArray = explode("-", $guid);
-                        $cmsId = $guidArray[0];
-                        $newName = $cmsId.".jpg";
-                        echo $newName;
-                        $ch = curl_init($image);
-                        $fileName = '/var/www/html/_retsapi/imagesStories/'.$newName;
-                        $fp = fopen($fileName, 'wb');
-                        curl_setopt($ch, CURLOPT_FILE, $fp);
-                        curl_setopt($ch, CURLOPT_HEADER, 0);
-                        curl_exec($ch);
-                        curl_close($ch);
-                        fclose($fp);
-                        $contents = file_get_contents($image);
-                        file_put_contents($fileName, $contents);
-                }
-        }
-        echo "</li>";
+                        $image = (string)$ns_media->content->attributes()['url'];
 
+                        echo " --- ".$image." ---";
+                        if ($image != "")
+                        {
+                                $guid = $entry->guid;
+                                $guidArray = explode("-", $guid);
+                                $cmsId = $guidArray[0];
+                                $newName = $cmsId.".jpg";
+                                echo $newName;
+                                $ch = curl_init($image);
+                                $fileName = '/var/www/html/_retsapi/imagesStories/'.$newName;
+                                $fp = fopen($fileName, 'wb');
+                                curl_setopt($ch, CURLOPT_FILE, $fp);
+                                curl_setopt($ch, CURLOPT_HEADER, 0);
+                                curl_exec($ch);
+                                curl_close($ch);
+                                fclose($fp);
+                                $contents = file_get_contents($image);
+                                file_put_contents($fileName, $contents);
+                        }
+                }
+                echo "</li>";
+
+        }
+        echo "</ul>";
 }
-echo "</ul>";
