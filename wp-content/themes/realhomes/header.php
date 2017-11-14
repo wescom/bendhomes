2344eb55f30ca5ae3ca0614d455b728dfc7b0ce0
jarelsc <jculley@wescompapers.com>
2017-11-14 09:32:40 -0800

    widget

--- wp-content/themes/realhomes/header.php
+++ wp-content/themes/realhomes/header.php
@@ -98,10 +98,11 @@ var trackOutboundLink=function(n,t){ga("send","event",t,"click",n,{transport:"be
 						<div class="user-nav clearfix">
 
 							<a id="menu-toggle" class="menu-control" href="#sidr"><i class="fa fa-bars"></i></a>
-
+							<?php /*
 							<a class="nav-advanced-search hidden-phone" href="http://bendhomes.idxbroker.com/idx/search/advanced">Advanced Search</a>
-							
 							<a class="nav-map-search hidden-phone" href="http://bendhomes.idxbroker.com/idx/map/mapsearch">Map Search</a>
+							*/
+							?>
 
 							<a class="nav-login" href="http://bendhomes.idxbroker.com/idx/userlogin">Login / Register</a>
 
@@ -181,7 +182,7 @@ var trackOutboundLink=function(n,t){ga("send","event",t,"click",n,{transport:"be
 
                 </header>
 				
-				<div class="header-search" style="border-top:1px solid #444;margin-top:10px;padding-bottom:10px;">
+				<div class="header-search" style="border-top:1px solid #444;margin-top:10px;padding-bottom:9px;">
 					<script type="text/javascript" id="idxwidgetsrc-41723" src="//bendhomes.idxbroker.com/idx/quicksearchjs.php?widgetid=41723"></script>
 					<div style="text-align:right;padding:0 1%;font-size:12px;font-weight:400;letter-spacing:.04em;">
 						<a href="http://bendhomes.idxbroker.com/idx/search/advanced" style="color:#929A9B">Advanced Search</a> &nbsp;| &nbsp;<a href="http://bendhomes.idxbroker.com/idx/map/mapsearch" style="color:#929A9B">Map Search</a>
