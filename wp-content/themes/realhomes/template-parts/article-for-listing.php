97c6851ea494f63858613fc46a7910a83f6a21aa
jarelsc <jculley@wescompapers.com>
2017-12-05 14:28:38 -0800

    excerpt

--- wp-content/themes/realhomes/template-parts/article-for-listing.php
+++ wp-content/themes/realhomes/template-parts/article-for-listing.php
@@ -22,7 +22,6 @@
                 </span>
             </small>
         </header>
-        <p><?php //framework_excerpt(30);  ?><?php echo str_replace( '&#013; ', '', get_the_excerpt() ); ?></p>
-		<div><a class="real-btn" href="<?php the_permalink(); ?>"><?php _e('Read more', 'framework'); ?></a></div>
+		<p><?php //framework_excerpt(30);  ?><?php echo str_replace( '&#013; ', '', get_the_excerpt() ); ?> <a href="<?php the_permalink(); ?>">Read more</a></p>
     </div>
 </article>
