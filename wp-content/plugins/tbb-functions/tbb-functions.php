302618979563221a753f5ea51c2cb41cf51d6157
jcontreras28 <janellec@bendbroadband.com>
2017-12-27 11:24:04 -0800

    working on map search button

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -216,6 +216,10 @@ function rets_footer_code() {
 		<script>
 		function agentRender(e){agentHtml=e.html,agentHtml=agentHtml.replace('"','"'),agentHtml=agentHtml.replace("/","/"),$(".sidebar").prepend(agentHtml)}function openHouseRender(e){opensHtml=e.html,opensHtml=opensHtml.replace('"','"'),opensHtml=opensHtml.replace("/","/"),$("#IDX-description").before(opensHtml)}$("#idx20817_42205-2 .title").hide();var theUrl=window.location.href,urlArray=theUrl.split("/"),mlsNum=urlArray[7],getAgentUrl="<?php echo TBB_FUNCTIONS_URL .'rets-agent-widget.php'; ?>?mls="+mlsNum,getOpensUrl="<?php echo TBB_FUNCTIONS_URL .'rets-openhouse-widget.php'; ?>?mls="+mlsNum;urlArray.length<8?$(".IDX-featuredAgentWrap").hide():$.ajax({url:getAgentUrl,jsonp:"agentRender",dataType:"jsonp",success:function(e){},error:function(){}}),$.ajax({url:getOpensUrl,jsonp:"openHouseRender",dataType:"jsonp",success:function(e){},error:function(){}});
 		</script>
+
+		<script>
+			console.log("im here");
+		</script>
 		
 	<?php }
 	
