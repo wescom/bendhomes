2398e1cda254f8f5c83efe97691e92495353a403
jcontreras28 <janellec@bendbroadband.com>
2017-12-27 13:19:23 -0800

    working on map search button

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -187,7 +187,7 @@ function rets_footer_code() {
 
 		function mapSearchButtonRender() {
 			console.log('inserting button');
-			var theButton = "<div class='IDX-btn IDX-btn-primary'><a href=''>Map Search</a></div>";
+			var theButton = "<div class='IDX-btn IDX-btn-primary' id='mapSearchButton'><a href=''>Map Search</a></div>";
 			$('#IDX-resultsRefineSubmit').after(theButton);
 		}
 		
@@ -224,7 +224,7 @@ function rets_footer_code() {
 		</script>*/
 		?>
 		<script>
-		function agentRender(e){agentHtml=e.html,agentHtml=agentHtml.replace('"','"'),agentHtml=agentHtml.replace("/","/"),$(".sidebar").prepend(agentHtml)}function openHouseRender(e){opensHtml=e.html,opensHtml=opensHtml.replace('"','"'),opensHtml=opensHtml.replace("/","/"),$("#IDX-description").before(opensHtml)}function mapSearchButtonRender(){console.log("inserting button");var e="<div class='IDX-btn IDX-btn-primary'><a href=''>Map Search</a></div>";$("#IDX-resultsRefineSubmit").after(e)}$("#idx20817_42205-2 .title").hide();var theUrl=window.location.href,urlArray=theUrl.split("/"),mlsNum=urlArray[7],getAgentUrl="<?php echo TBB_FUNCTIONS_URL .'rets-agent-widget.php'; ?>?mls="+mlsNum,getOpensUrl="<?php echo TBB_FUNCTIONS_URL .'rets-openhouse-widget.php'; ?>?mls="+mlsNum;urlArray.length<8?$(".IDX-featuredAgentWrap").hide():$.ajax({url:getAgentUrl,jsonp:"agentRender",dataType:"jsonp",success:function(e){},error:function(){}}),$.ajax({url:getOpensUrl,jsonp:"openHouseRender",dataType:"jsonp",success:function(e){},error:function(){}}),mapSearchButtonRender();
+		function agentRender(e){agentHtml=e.html,agentHtml=agentHtml.replace('"','"'),agentHtml=agentHtml.replace("/","/"),$(".sidebar").prepend(agentHtml)}function openHouseRender(e){opensHtml=e.html,opensHtml=opensHtml.replace('"','"'),opensHtml=opensHtml.replace("/","/"),$("#IDX-description").before(opensHtml)}function mapSearchButtonRender(){console.log("inserting button");var e="<div class='IDX-btn IDX-btn-primary' id='mapSearchButton'><a href=''>Map Search</a></div>";$("#IDX-resultsRefineSubmit").after(e)}$("#idx20817_42205-2 .title").hide();var theUrl=window.location.href,urlArray=theUrl.split("/"),mlsNum=urlArray[7],getAgentUrl="<?php echo TBB_FUNCTIONS_URL .'rets-agent-widget.php'; ?>?mls="+mlsNum,getOpensUrl="<?php echo TBB_FUNCTIONS_URL .'rets-openhouse-widget.php'; ?>?mls="+mlsNum;urlArray.length<8?$(".IDX-featuredAgentWrap").hide():$.ajax({url:getAgentUrl,jsonp:"agentRender",dataType:"jsonp",success:function(e){},error:function(){}}),$.ajax({url:getOpensUrl,jsonp:"openHouseRender",dataType:"jsonp",success:function(e){},error:function(){}}),mapSearchButtonRender();
 		</script>
 		
 	<?php }
