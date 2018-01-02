9d1c762a9b0bec7380afe1b456d3cc71ea384163
jcontreras28 <janellec@bendbroadband.com>
2018-01-02 14:46:03 -0800

    map view button

--- wp-content/plugins/tbb-functions/tbb-functions.php
+++ wp-content/plugins/tbb-functions/tbb-functions.php
@@ -192,10 +192,15 @@ function rets_footer_code() {
 		}
 
 		console.log('in tbb-functions');
-		$(window).load(function () {
-			console.log('calling mapSearchButtonRender');
-			mapSearchButtonRender();
-		});
+		// Polling for the window to be loaded
+		var interval = setInterval(function() {
+		    if(document.readyState === 'complete') {
+		        clearInterval(interval);
+		        console.log('calling mapSearchButtonRender');
+				mapSearchButtonRender();
+		        done();
+		    }    
+		}, 100);
 		
 		$('#idx20817_42205-2 .title').hide();
 		var theUrl = window.location.href,
@@ -233,7 +238,7 @@ function rets_footer_code() {
 		</script>*/
 		?>
 		<script>
-		function agentRender(e){agentHtml=e.html,agentHtml=agentHtml.replace('"','"'),agentHtml=agentHtml.replace("/","/"),$(".sidebar").prepend(agentHtml)}function openHouseRender(e){opensHtml=e.html,opensHtml=opensHtml.replace('"','"'),opensHtml=opensHtml.replace("/","/"),$("#IDX-description").before(opensHtml)}function mapSearchButtonRender(){console.log("inserting button");var e="<div class='IDX-btn IDX-btn-primary' id='mapSearchButton'><a href='http://bendhomes.idxbroker.com/idx/map/mapsearch'>Map Search</a></div>";$("#IDX-resultsRefineSubmit").after(e)}console.log("in tbb-functions"),$(window).load(function(){console.log("calling mapSearchButtonRender"),mapSearchButtonRender()}),$("#idx20817_42205-2 .title").hide();var theUrl=window.location.href,urlArray=theUrl.split("/"),mlsNum=urlArray[7],getAgentUrl="<?php echo TBB_FUNCTIONS_URL .'rets-agent-widget.php'; ?>?mls="+mlsNum,getOpensUrl="<?php echo TBB_FUNCTIONS_URL .'rets-openhouse-widget.php'; ?>?mls="+mlsNum;theUrl.indexOf("details/listing")>0&&(urlArray.length<8?$(".IDX-featuredAgentWrap").hide():$.ajax({url:getAgentUrl,jsonp:"agentRender",dataType:"jsonp",success:function(e){},error:function(){}})),$.ajax({url:getOpensUrl,jsonp:"openHouseRender",dataType:"jsonp",success:function(e){},error:function(){}});
+		function agentRender(e){agentHtml=e.html,agentHtml=agentHtml.replace('"','"'),agentHtml=agentHtml.replace("/","/"),$(".sidebar").prepend(agentHtml)}function openHouseRender(e){opensHtml=e.html,opensHtml=opensHtml.replace('"','"'),opensHtml=opensHtml.replace("/","/"),$("#IDX-description").before(opensHtml)}function mapSearchButtonRender(){console.log("inserting button");var e="<div class='IDX-btn IDX-btn-primary' id='mapSearchButton'><a href='http://bendhomes.idxbroker.com/idx/map/mapsearch'>Map Search</a></div>";$("#IDX-resultsRefineSubmit").after(e)}console.log("in tbb-functions");var interval=setInterval(function(){"complete"===document.readyState&&(clearInterval(interval),console.log("calling mapSearchButtonRender"),mapSearchButtonRender(),done())},100);$("#idx20817_42205-2 .title").hide();var theUrl=window.location.href,urlArray=theUrl.split("/"),mlsNum=urlArray[7],getAgentUrl="<?php echo TBB_FUNCTIONS_URL .'rets-agent-widget.php'; ?>?mls="+mlsNum,getOpensUrl="<?php echo TBB_FUNCTIONS_URL .'rets-openhouse-widget.php'; ?>?mls="+mlsNum;theUrl.indexOf("details/listing")>0&&(urlArray.length<8?$(".IDX-featuredAgentWrap").hide():$.ajax({url:getAgentUrl,jsonp:"agentRender",dataType:"jsonp",success:function(e){},error:function(){}})),$.ajax({url:getOpensUrl,jsonp:"openHouseRender",dataType:"jsonp",success:function(e){},error:function(){}});
 		</script>
 		
 	<?php }
