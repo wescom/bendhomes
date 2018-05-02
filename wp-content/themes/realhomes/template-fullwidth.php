c1cd385815cc036dbdb1f145da34d6e807d7af40
jcontreras28 <janellec@bendbroadband.com>
2018-05-02 14:07:38 -0700

    rework of home page

--- wp-content/themes/realhomes/template-fullwidth.php
+++ wp-content/themes/realhomes/template-fullwidth.php
@@ -116,7 +116,7 @@ if( is_front_page() ) {
 						foreach ( $recent_posts as $post ) {
 							$catArray = get_the_category($post['ID']);
 							$catName = "stuff";
-							var_dump(to_array($catArray[0]));
+							var_dump($catArray[0].to_array());
                 		?>
              
 						<div class="story-item clearfix category-1829475">
