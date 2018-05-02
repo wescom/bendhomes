cde22ee29e5a7851da94a49341283829486a6c0f
jcontreras28 <janellec@bendbroadband.com>
2018-05-02 09:44:47 -0700

    rework of home page

--- wp-content/themes/realhomes/template-fullwidth.php
+++ wp-content/themes/realhomes/template-fullwidth.php
@@ -105,8 +105,26 @@ if( is_front_page() ) {
 						</div>
 					</div>
 				</div>
-				<div class="col-sm-6">
-					Story list here
+				<div class="col-sm-6 relative">
+					<div class="story-loop-wrap top-stories-list">
+						<div class="story-item clearfix category-1829475">
+						
+					
+							<a href="http://www.bendbulletin.com/business/6208285-151/redmond-airport-travelers-need-to-remove-electronics-for?referrer=home&amp;referrer=top">
+								<img src="/csp/mediapool/sites/dt.common.streams.StreamServer.cls?STREAMOID=QS8hYYIS7pTIsC8w4x6ucc$daE2N3K4ZzOUsqbU5sYsXgo6amXLEQ9mb4ncBKZHKWCsjLu883Ygn4B49Lvm9bPe2QeMKQdVeZmXF$9l$4uCZ8QDXhaHEp3rvzXRJFdy0KqPHLoMevcTLo3h8xh70Y6N_U_CryOsw6FTOdKL_jpQ-&amp;CONTENTTYPE=image/jpeg" class="pull-right" alt="" width="140" height="93">
+							</a>
+								
+							<div class="section"><small><a href="/business?referrer=topstory" class="color-darkgray">business</a></small></div>	
+							<h2>
+								<a href="http://www.bendbulletin.com/business/6208285-151/redmond-airport-travelers-need-to-remove-electronics-for?referrer=home&amp;referrer=top">Travelers must remove electronics from carry-on bags</a>
+							</h2>
+							<cite class="author-wrap">		
+								<div class="pop">		
+									<span><a href="http://www.bendbulletin.com/NewsroomStaffList/?person=159">Kathleen McLaughlin</a>, The Bulletin</span>
+								</div>
+							</cite>
+						</div>
+					</div>
 				</div>
 			</div>
 
