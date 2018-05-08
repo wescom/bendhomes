70b7bcecb6c97ae9d59aee55a53139c8f8288e67
jcontreras28 <janellec@bendbroadband.com>
2018-05-08 10:13:21 -0700

    adding json ld

--- wp-content/themes/realhomes/json-ld.php
+++ wp-content/themes/realhomes/json-ld.php
@@ -35,7 +35,7 @@ if (is_front_page()) {
 	$payload["name"] = "Bendhomes ".$post_data->post_title; 
 	$payload["url"] = "http://www.bendhomes.com".$_SERVER['REQUEST_URI']; 
 	$payload["sameAs"] = array( "https://twitter.com/BendHomes541", "https://www.facebook.com/bendhomes541/", "https://plus.google.com/101058950766867205838" ); 
-	$payload["publisher"] = array( array( "@type" => "Organization", "telephone" => "541 382 1811", "email" => "info@bendhomes.com", "contactType" => "sales", "logo" => "http://www.bendhomes.com/wp-content/uploads/2017/12/BendHomes.comLogoArt.png"  ) ); 
+	$payload["publisher"] = array( array( "@type" => "Organization", "telephone" => "541 382 1811", "email" => "info@bendhomes.com", "logo" => "http://www.bendhomes.com/wp-content/uploads/2017/12/BendHomes.comLogoArt.png"  ) ); 
 	$payload["identifier"] = $post_data->post_title;
 } 
 
