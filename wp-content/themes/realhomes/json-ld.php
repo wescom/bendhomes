1d6bfc3c105896f27b3fe770845a7fdd46b46d35
jcontreras28 <janellec@bendbroadband.com>
2018-05-08 10:32:39 -0700

    adding json ld

--- wp-content/themes/realhomes/json-ld.php
+++ wp-content/themes/realhomes/json-ld.php
@@ -26,7 +26,7 @@ if (is_single()) { // this gets the data for the user who wrote that particular 
 	$payload["image"] = $post_thumb; 
 	$payload["ArticleSection"] = $category[0]->cat_name; 
 	$payload["identifier"] = $post_data->ID;
-	$payload["publisher"] = array( array( "@type" => "Organization", "telephone" => "541 382 1811", "email" => "info@bendhomes.com", "name" => "Bendhomes", "logo" => array("src" => "http://www.bendhomes.com/wp-content/uploads/2017/12/BendHomes.comLogoArt.png")  ) ); 
+	$payload["publisher"] = array( array( "@type" => "Organization", "telephone" => "541 382 1811", "email" => "info@bendhomes.com", "name" => "Bendhomes", "logo" => array("@type" => "ImageObject", "url" => "http://www.bendhomes.com/wp-content/uploads/2017/12/BendHomes.comLogoArt.png")  ) ); 
 
 } // we do all this separately so we keep the right things for organization together 
 
